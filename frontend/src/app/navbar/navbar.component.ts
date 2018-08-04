c5f0320f99124e6af43e75991b536ebf08f10bc3
Maxence-Leroy <maxence.leroy3@gmail.com>
2018-08-04 11:26:13 -0400

    Use regular expression for the navbar

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -49,7 +49,8 @@ export class NavbarComponent implements OnInit {
     }
 
     isActive(url){
-        return [url].includes(this.router.url);
+		var regex = new RegExp(url);
+        return regex.test(this.router.url);
     }
 
 }
