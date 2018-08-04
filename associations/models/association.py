bce63f4c30a5f6c159c7b36688bf6e38dbb93288
Maxence-Leroy <maxence.leroy3@gmail.com>
2018-08-04 00:05:25 -0400

    Allow to disable market, library and publication

--- associations/models/association.py
+++ associations/models/association.py
@@ -6,6 +6,20 @@ from associations.models.library import Library
 from associations.models.publication import Publication
 from authentication.models import User
 
+class Group(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    members = models.ManyToManyField(User, blank=True)
+
+    is_admin_group = models.BooleanField(default=False)
+
+    # has editing rights on :
+    static_page = models.BooleanField(default=False)
+    news = models.BooleanField(default=False)
+    marketplace = models.BooleanField(default=False)
+    library = models.BooleanField(default=False)
+    vote = models.BooleanField(default=False)
+    events = models.BooleanField(default=False)
 
 class Association(models.Model):
 
@@ -16,13 +30,15 @@ class Association(models.Model):
 
     logo = models.ImageField(upload_to="associations/logos/", null=True)
 
-    market = models.OneToOneField(Marketplace, on_delete=models.DO_NOTHING)
-    library = models.OneToOneField(Library, on_delete=models.DO_NOTHING)
-    publication = models.OneToOneField(Publication, on_delete=models.DO_NOTHING)
+    market = models.OneToOneField(Marketplace, on_delete=models.DO_NOTHING, null = True)
+    library = models.OneToOneField(Library, on_delete=models.DO_NOTHING, null = True)
+    publication = models.OneToOneField(Publication, on_delete=models.DO_NOTHING, null = True)
 
     is_hidden_1A = models.BooleanField(default=False, verbose_name="Cachée aux 1A")
     rank = models.IntegerField(default=0,
                                help_text="Ordre d'apparition dans la liste des associations (ordre alphabétique pour les valeurs égales)")
+
+    groups = models.ManyToManyField(Group, blank=True)
 
     def _get_unique_slug(self):
         slug = slugify(self.name)
@@ -38,18 +54,3 @@ class Association(models.Model):
             self.slug = self._get_unique_slug()
         super().save(*args, **kwargs)
 
-
-class Group(models.Model):
-    id = models.AutoField(primary_key=True)
-
-    members = models.ManyToManyField(User, blank=True)
-
-    is_admin_group = models.BooleanField(default=False)
-
-    # has editing rights on :
-    static_page = models.BooleanField(default=False)
-    news = models.BooleanField(default=False)
-    marketplace = models.BooleanField(default=False)
-    library = models.BooleanField(default=False)
-    vote = models.BooleanField(default=False)
-    events = models.BooleanField(default=False)
