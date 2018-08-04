d13bee15a5304f29145ce6dc82b99075bf761338
Maxence-Leroy <maxence.leroy3@gmail.com>
2018-08-04 16:39:11 -0400

    Fix syntax error

--- frontend/src/app/associations/association-homepage/association-homepage.component.ts
+++ frontend/src/app/associations/association-homepage/association-homepage.component.ts
@@ -22,7 +22,7 @@ export class AssociationHomepageComponent implements OnInit {
 		this.route.params.subscribe((params:Params) => {
         this.id = params['id'];
         this.customInit();
-		}
+		});
     }
 	
 	customInit(){
