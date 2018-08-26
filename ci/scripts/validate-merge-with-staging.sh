ae06156cb492534c71865e43aca78b0024f07bf0
Florian Veaux <florian.veaux@mines-paristech.fr>
2018-08-26 17:59:34 +0200

    Fix typo in a ci script

--- ci/scripts/validate-merge-with-staging.sh
+++ ci/scripts/validate-merge-with-staging.sh
@@ -8,7 +8,7 @@ git fetch --quiet --no-tags origin master
 git fetch --no-tags --force origin staging:staging
 git diff
 git checkout staging
-git config --global user.email "gitlag@mines-paris.eu"
+git config --global user.email "gitlab@mines-paris.eu"
 git config --global user.name "GITLAB CI"
 env EDITOR=cat git merge --no-ff "$CI_COMMIT_SHA"
 
