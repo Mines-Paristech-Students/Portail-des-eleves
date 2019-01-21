bfa544951d9086a54d6006ac47dd592e6840532a
Florian Veaux <florian.veaux@mines-paristech.fr>
2019-01-21 15:39:47 +0100

    Fix tests

--- polls/tests.py
+++ polls/tests.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import timedelta, date
 
 from django.utils import timezone
 from django.urls import reverse
@@ -6,9 +6,9 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.exceptions import NotAuthenticated
 
-from backend.tests import BackendTestCase
+from backend.tests_utils import BackendTestCase
 from authentication.models import User
-from .models import Poll, Choice
+from polls.models import Poll, Choice
 
 
 class PollTestCase(BackendTestCase):
@@ -36,7 +36,7 @@ class PollTestCase(BackendTestCase):
 
         response = self.client.post(url, data, format='json')
 
-        if not silent and response.status_code == status.HTTP_403_FORBIDDEN:
+        if not silent and response.status_code == status.HTTP_401_UNAUTHORIZED:
             raise NotAuthenticated()
 
         return response
@@ -53,7 +53,7 @@ class PollTestCase(BackendTestCase):
             silent
         )
 
-    def create_poll(self, question, user_id, choices=(), state='ACCEPTED', start_date_offset=-10, end_date_offset=+10):
+    def create_poll(self, question, user_id, choices=(), state='ACCEPTED', start_date_offset=-10):
         """
         Create a poll.
 
@@ -61,19 +61,17 @@ class PollTestCase(BackendTestCase):
         :param str user_id: the id of the author.
         :param list choices: a list of texts for the choices.
         :param str state: choose among ('REVIEWING', 'REJECTED', 'ACCEPTED')
-        :param int start_date_offset: the number of days as publication_date_time = now() + start_date_offset
-        :param int end_date_offset: the number of days as publication_end_date_time = now() + end_date_offset
+        :param int start_date_offset: the number of days as publication_date = now() + start_date_offset
         :return Poll: the created poll object.
         """
 
-        now = timezone.now()
+        now = date.today()
 
         poll = Poll.objects.create(
             user=User.objects.get(id=user_id),
             question=question,
             state=state,
-            publication_date_time=now + timedelta(days=start_date_offset),
-            publication_end_date_time=now + timedelta(days=end_date_offset)
+            publication_date=now + timedelta(days=start_date_offset),
         )
 
         for choice_text in choices:
@@ -87,89 +85,42 @@ class PollTestCase(BackendTestCase):
 
 class PollModelTest(PollTestCase):
     def test_poll_was_published(self):
-        """is_published() returns False for a poll whose publication_end_date_time is over."""
-        old_time = timezone.now() - timedelta(days=1)
-        old_poll = Poll(publication_date_time=old_time - timedelta(days=1), publication_end_date_time=old_time)
-        self.assertFalse(old_poll.is_published())
+        """has_been_published() returns True for a old poll"""
+        old_time = date.today() - timedelta(days=1)
+        old_poll = Poll(state='ACCEPTED', publication_date=old_time - timedelta(days=1))
+        self.assertTrue(old_poll.has_been_published())
 
     def test_accepted_poll_is_published(self):
         """
-        is_published() returns True for a poll whose state is ACCEPTED and whose publication_date_time and
-        publication_end_date_time are before and after now().
+        has_been_published() returns True for a poll whose state is ACCEPTED and whose publication_date is <= today
         """
-        old_time = timezone.now() - timedelta(days=1)
-        future_time = timezone.now() + timedelta(days=1)
-        poll = Poll(state='ACCEPTED', publication_date_time=old_time, publication_end_date_time=future_time)
-        self.assertTrue(poll.is_published())
+        old_time = date.today() - timedelta(days=1)
+        poll = Poll(state='ACCEPTED', publication_date=old_time)
+        self.assertTrue(poll.has_been_published())
 
     def test_poll_will_be_published(self):
-        """is_published() returns False for a poll whose publication_date_time is set on the future."""
-        future_time = timezone.now() + timedelta(days=1)
-        future_poll = Poll(publication_date_time=future_time)
-        self.assertFalse(future_poll.is_published())
+        """has_been_published() returns False for a poll whose publication_date is set on the future."""
+        future_time = date.today() + timedelta(days=1)
+        future_poll = Poll(publication_date=future_time)
+        self.assertFalse(future_poll.has_been_published())
 
     def test_poll_rejected_state(self):
-        """is_published() returns False for a poll whose state is REJECTED."""
-        old_time = timezone.now() - timedelta(days=1)
-        future_time = timezone.now() + timedelta(days=1)
-        poll = Poll(state='REJECTED', publication_date_time=old_time, publication_end_date_time=future_time)
-        self.assertFalse(poll.is_published())
+        """has_been_published() returns False for a poll whose state is REJECTED."""
+        old_time = date.today() - timedelta(days=1)
+        poll = Poll(state='REJECTED', publication_date=old_time)
+        self.assertFalse(poll.has_been_published())
 
     def test_poll_reviewing_state(self):
-        """is_published() returns False for a poll whose state is REVIEWING."""
-        old_time = timezone.now() - timedelta(days=1)
-        future_time = timezone.now() + timedelta(days=1)
-        poll = Poll(state='REVIEWING', publication_date_time=old_time, publication_end_date_time=future_time)
-        self.assertFalse(poll.is_published())
+        """is_publhas_been_publishedished() returns False for a poll whose state is REVIEWING."""
+        old_time = date.today() - timedelta(days=1)
+        poll = Poll(state='REVIEWING', publication_date=old_time)
+        self.assertFalse(poll.has_been_published())
 
     def test_poll_no_state(self):
-        """is_published() returns False for a poll which has no state."""
-        old_time = timezone.now() - timedelta(days=1)
-        future_time = timezone.now() + timedelta(days=1)
-        poll = Poll(publication_date_time=old_time, publication_end_date_time=future_time)
-        self.assertFalse(poll.is_published())
-
-
-class ListCurrentPollsTest(PollTestCase):
-    def test_not_authenticated(self):
-        """Return 403 as the user is not authenticated."""
-        response = self.client.get(reverse('polls-current'))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_no_polls(self):
-        """Return 200 and an empty polls list."""
-        self.create_and_login_user(admin=False)
-
-        response = self.client.get(reverse('polls-current'))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, [])
-
-    def test_with_accepted_polls_different_dates(self):
-        """Return 200 and return only the restricted version of the currently published polls."""
-        self.create_and_login_user(id='admin_user', admin=False)
-        self.create_poll('Old question', user_id='admin_user', state='ACCEPTED', start_date_offset=-10,
-                         end_date_offset=-9, choices=self.DUMMY_CHOICES)
-        self.create_poll('Current question', user_id='admin_user', state='ACCEPTED', start_date_offset=-10,
-                         end_date_offset=10, choices=self.DUMMY_CHOICES)
-        self.create_poll('Future question', user_id='admin_user', state='ACCEPTED', start_date_offset=10,
-                         end_date_offset=20, choices=self.DUMMY_CHOICES)
-
-        response = self.client.get(reverse('polls-current'))
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertNotContains(response, 'Old question')
-        self.assertNotContains(response, 'Future question')
-        self.assertContains(response, 'Current question')
-
-        # Test the returned attributes.
-        self.assertSetEqual(
-            set(response.data[0].keys()),
-            {'url', 'choices', 'question'}
-        )
-        self.assertSetEqual(
-            set(response.data[0]['choices'][0].keys()),
-            {'text', 'poll'}
-        )
+        """has_been_published() returns False for a poll which has no state."""
+        old_time = date.today() - timedelta(days=1)
+        poll = Poll(publication_date=old_time)
+        self.assertFalse(poll.has_been_published())
 
 
 class ListSubmittedPollsTest(PollTestCase):
@@ -177,7 +128,7 @@ class ListSubmittedPollsTest(PollTestCase):
         """Return 403 as the user is not authenticated."""
         response = self.client.get(reverse('polls-submitted'))
 
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_no_polls(self):
         """Return 200 and an empty polls list."""
@@ -208,12 +159,12 @@ class ListSubmittedPollsTest(PollTestCase):
         self.assertNotContains(response_1, 'user_2')
         self.assertSetEqual(
             set(response_1.data[0].keys()),
-            {'url', 'question', 'user', 'creation_date_time', 'state', 'publication_date_time',
-             'publication_end_date_time', 'admin_comment', 'choices'}
+            {'id', 'url', 'question', 'user', 'creation_date_time', 'state', 'publication_date',
+             'admin_comment', 'choices'}
         )
         self.assertSetEqual(
             set(response_1.data[0]['choices'][0].keys()),
-            {'text', 'poll'}
+            {'id', 'text', 'poll'}
         )
 
         # Test user 2
@@ -226,12 +177,12 @@ class ListSubmittedPollsTest(PollTestCase):
         self.assertNotContains(response_2, 'user_1')
         self.assertSetEqual(
             set(response_2.data[0].keys()),
-            {'url', 'question', 'user', 'creation_date_time', 'state', 'publication_date_time',
-             'publication_end_date_time', 'admin_comment', 'choices'}
+            {'id', 'url', 'question', 'user', 'creation_date_time', 'state', 'publication_date',
+             'admin_comment', 'choices'}
         )
         self.assertSetEqual(
             set(response_2.data[0]['choices'][0].keys()),
-            {'text', 'poll'}
+            {'id', 'text', 'poll'}
         )
 
 
@@ -268,12 +219,12 @@ class ListAllPollsTest(PollTestCase):
         self.assertContains(response, 'user_2')
         self.assertSetEqual(
             set(response.data[0].keys()),
-            {'url', 'question', 'user', 'creation_date_time', 'state', 'publication_date_time',
-             'publication_end_date_time', 'admin_comment', 'choices'}
+            {'id', 'url', 'question', 'user', 'creation_date_time', 'state', 'publication_date',
+             'admin_comment', 'choices'}
         )
         self.assertSetEqual(
             set(response.data[0]['choices'][0].keys()),
-            {'text', 'poll'}
+            {'id', 'text', 'poll'}
         )
 
 
@@ -287,7 +238,7 @@ class RetrievePollTest(PollTestCase):
         poll_id = Poll.objects.latest('creation_date_time').id
         response = self.client.get(reverse('polls-retrieve', kwargs={'id': poll_id}))
 
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_unknown_poll(self):
         """Return 404."""
@@ -311,20 +262,19 @@ class RetrievePollTest(PollTestCase):
             self.assertContains(response, choice)
         self.assertSetEqual(
             set(response.data.keys()),
-            {'url', 'question', 'user', 'creation_date_time', 'state', 'publication_date_time',
-             'publication_end_date_time', 'admin_comment', 'choices'}
+            {'id', 'url', 'question', 'user', 'creation_date_time', 'state', 'publication_date',
+             'admin_comment', 'choices'}
         )
         self.assertSetEqual(
             set(response.data['choices'][0].keys()),
-            {'text', 'poll'}
+            {'id', 'text', 'poll'}
         )
 
     def test_simple_user_published_poll(self):
         """Return 200 and the restricted version of a poll submitted by another user."""
         # A first user create the poll. Directly set its state as ACCEPTED.
         self.create_user(id='author', admin=False)
-        self.create_poll(self.DUMMY_QUESTION, 'author', self.DUMMY_CHOICES, 'ACCEPTED', start_date_offset=-10,
-                         end_date_offset=10)
+        self.create_poll(self.DUMMY_QUESTION, 'author', self.DUMMY_CHOICES, 'ACCEPTED', start_date_offset=-10)
         self.logout()
 
         # Login as a different user and retrieve the poll.
@@ -339,11 +289,11 @@ class RetrievePollTest(PollTestCase):
             self.assertContains(response, choice)
         self.assertSetEqual(
             set(response.data.keys()),
-            {'url', 'question', 'choices'}
+            {'id', 'url', 'question', 'choices'}
         )
         self.assertSetEqual(
             set(response.data['choices'][0].keys()),
-            {'text', 'poll'}
+            {'id', 'text', 'poll'}
         )
 
     def test_simple_user_not_published_poll(self):
@@ -380,12 +330,12 @@ class RetrievePollTest(PollTestCase):
             self.assertContains(response, choice)
         self.assertSetEqual(
             set(response.data.keys()),
-            {'url', 'question', 'user', 'creation_date_time', 'state', 'publication_date_time',
-             'publication_end_date_time', 'admin_comment', 'choices'}
+            {'id', 'url', 'question', 'user', 'creation_date_time', 'state', 'publication_date',
+             'admin_comment', 'choices'}
         )
         self.assertSetEqual(
             set(response.data['choices'][0].keys()),
-            {'text', 'poll'}
+            {'id', 'text', 'poll'}
         )
 
 
@@ -397,7 +347,7 @@ class SubmitPollTest(PollTestCase):
         """
         response = self.submit_dummy_poll(silent=True)
 
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(Poll.objects.count(), 0)
 
     def test_submit_poll(self):
@@ -470,7 +420,7 @@ class UpdatePollTest(PollTestCase):
             data,
             format='json',
         )
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_not_admin(self):
         """Return 403 as the user is not an admin."""
@@ -542,8 +492,7 @@ class UpdatePollTest(PollTestCase):
         poll_id = Poll.objects.latest('creation_date_time').id
         url = reverse('polls-update', kwargs={'id': poll_id})
         data = {
-            'publication_date_time': '2018-01-01 12:00',
-            'publication_end_date_time': '2018-01-02 12:00',
+            'publication_date': date(day=1, month=1, year=2018)
         }
         response = self.client.patch(
             url,
@@ -554,10 +503,6 @@ class UpdatePollTest(PollTestCase):
         # Test.
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(
-            Poll.objects.get(id=poll_id).publication_date_time.strftime('%Y-%m-%d %H:%M'),
-            data['publication_date_time'],
-        )
-        self.assertEqual(
-            Poll.objects.get(id=poll_id).publication_end_date_time.strftime('%Y-%m-%d %H:%M'),
-            data['publication_end_date_time'],
+            Poll.objects.get(id=poll_id).publication_date,
+            data['publication_date'],
         )
