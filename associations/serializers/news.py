89107d44220bde70c3a113e8b7642d66f8c08204
Florian Veaux <florian.veaux@mines-paristech.fr>
2019-01-24 12:10:41 +0100

    Fully functional news for asso (no news feed yet)

--- associations/serializers/news.py
+++ associations/serializers/news.py
@@ -4,6 +4,21 @@ from associations.models import News
 
 
 class NewsSerializer(serializers.ModelSerializer):
+
+    author = serializers.PrimaryKeyRelatedField(
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+
     class Meta:
         model = News
-        fields = ("id", "title", "text", "association")
+        fields = ("id", "author", "title", "text", "association")
+
+
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        validated_data['author'] = self.context['request'].user
+        return super().update(instance, validated_data)
