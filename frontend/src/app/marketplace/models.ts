caab62ccef4498ec3ee33c25774a5a32227f0420
14alarro <arnaud.larroche@mines-paristech.fr>
2019-02-26 12:59:00 +0100

    Add a lot of types to marketplace

--- frontend/src/app/marketplace/models.ts
+++ frontend/src/app/marketplace/models.ts
@@ -2,6 +2,7 @@ export interface RawProduct {
     id: number,
     marketplace: string,
     name: string,
+    description: string,
     number_left: number,
     orderable_online: boolean,
     price: string,
@@ -12,6 +13,7 @@ export class Product {
     id: number;
     marketplace: string;
     name: string;
+    description: string;
     number_left: number;
     orderable_online: boolean;
     price: number;
@@ -21,6 +23,7 @@ export class Product {
         this.id = rawProduct.id;
         this.marketplace = rawProduct.marketplace;
         this.name = rawProduct.name;
+        this.description = rawProduct.description;
         this.number_left = rawProduct.number_left;
         this.orderable_online = rawProduct.orderable_online;
         this.price = Number(rawProduct.price);
@@ -36,5 +39,46 @@ export class BasketItem {
     constructor (product: Product, quantity: number = 1) {
         this.product = product;
         this.quantity = quantity;
+    }
+}
+
+export type Basket = { [ marketplaceId: string ] : { [ productId: number ] : number } }
+
+export interface RawAssociationShort {
+    id: string,
+    logo: string,
+    name: string,
+}
+
+export class AssociationShort {
+    id: string;
+    logo: string;
+    name: string;
+
+    constructor(rawAssociationShort: RawAssociationShort) {
+        this.id = rawAssociationShort.id;
+        this.logo = rawAssociationShort.logo;
+        this.name = rawAssociationShort.name;
+    }
+}
+
+export interface RawMarketplace {
+    id: string,
+    enabled: boolean,
+    association: RawAssociationShort,
+    products: RawProduct[],
+}
+
+export class Marketplace {
+    id: string;
+    enabled: boolean;
+    association: AssociationShort;
+    products: Product[];
+
+    constructor(rawMarketplace: RawMarketplace) {
+        this.id = rawMarketplace.id;
+        this.enabled = rawMarketplace.enabled;
+        this.association = new AssociationShort(rawMarketplace.association);
+        this.products = rawMarketplace.products.map(rawProduct => new Product(rawProduct));
     }
 }
