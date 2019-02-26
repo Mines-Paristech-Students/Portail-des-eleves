ddd767925078a3c7344c4592a3d123c793199b30
Maxence-Leroy <maxence.leroy3@gmail.com>
2019-02-27 08:44:30 +1100

    Forum : Switch to markdown editor

--- frontend/src/app/forum/view-topic/view-topic.component.ts
+++ frontend/src/app/forum/view-topic/view-topic.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ApiService} from "../../api.service";
 import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
 import {Theme, Topic, MessageForum} from "../../models/forum";
+import { MarkdownService } from 'ngx-markdown';
+import { EditorInstance, EditorOption } from 'angular-markdown-editor';
 
 @Component({
   selector: 'app-view-topic',
@@ -18,13 +20,13 @@ export class ViewTopicComponent implements OnInit {
     error: any ;
 
 	answer_message: any;
+	editorOptions: any;
+	answer_message_editor: EditorInstance;
 
-	/*froalaOptions = {
-        pluginsEnabled: ["align", "colors", "draggable", "embedly", "emoticons", "entities", "lineBreaker", "link", "lists", "paragraphFormat", "paragraphStyle", "quickInsert", "quote", "save", "table", "url", "wordPaste"],
-        //pluginsEnabled: ["align", "colors", "draggable", "embedly", "emoticons", "entities", "file", "image", "imageManager", "lineBreaker", "link", "lists", "paragraphFormat", "paragraphStyle", "quickInsert", "quote", "save", "table", "url", "video", "wordPaste"],
-    };*/
-
-    constructor(private api: ApiService, private route: ActivatedRoute, private router: Router){}
+    constructor(private api: ApiService,
+				private route: ActivatedRoute,
+				private router: Router,
+				private markdownService: MarkdownService){}
 
     ngOnInit() {
 		this.topic = new Topic();
@@ -34,6 +36,11 @@ export class ViewTopicComponent implements OnInit {
 			this.topic_id = params['topic'];
 			this.customInit();
 		});
+		
+        this.editorOptions = {
+			onShow: (e) => this.answer_message_editor = e,
+			parser: (val) => this.markdownService.compile(val.trim())
+		};
 	}
 
 	customInit() {
@@ -73,7 +80,7 @@ export class ViewTopicComponent implements OnInit {
 				let message = new MessageForum();
                 Object.assign(message, data);
 				this.list_messages.push(message);
-				this.answer_message = "";
+				this.answer_message_editor.setContent("");
             },
             err => this.error = err.message
         )
