5f26c2cc62146697477398c8c34b577402dc40d7
Adrien Bocquet <adrienbocquet38@gmail.com>
2019-02-27 22:07:27 +0100

    Added search in fronted

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,191 +1,194 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule, LOCALE_ID } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { ApiService } from "./api.service";
-import { CookieService } from "ngx-cookie-service";
-import { registerLocaleData } from "@angular/common";
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule, LOCALE_ID} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {ApiService} from "./api.service";
+import {CookieService} from "ngx-cookie-service";
+import {registerLocaleData} from "@angular/common";
 import localeFr from "@angular/common/locales/fr";
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import {HttpClientModule, HTTP_INTERCEPTORS} from "@angular/common/http";
 
-import { AngularMarkdownEditorModule } from "angular-markdown-editor";
-import { MarkdownModule, MarkedOptions } from "ngx-markdown";
+import {AngularMarkdownEditorModule} from "angular-markdown-editor";
+import {MarkdownModule, MarkedOptions} from "ngx-markdown";
 
-import { NgSelectModule } from "@ng-select/ng-select";
-import { NgxPaginationModule } from "ngx-pagination";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { DragulaModule } from "ng2-dragula";
-import { NotifierModule, NotifierOptions } from 'angular-notifier';
+import {NgSelectModule} from "@ng-select/ng-select";
+import {NgxPaginationModule} from "ngx-pagination";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {DragulaModule} from "ng2-dragula";
+import {NotifierModule, NotifierOptions} from 'angular-notifier';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
-import { LoginComponent } from "./login/login.component";
-import { NavbarComponent } from "./navbar/navbar.component";
-import { AllAssociationsComponent } from "./associations/all-associations/all-associations.component";
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
+import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
+import {NavbarComponent} from "./navbar/navbar.component";
+import {AllAssociationsComponent} from "./associations/all-associations/all-associations.component";
 
-import { ProfileShowComponent } from "./profile/profile-show/profile-show.component";
-import { ProfileEditComponent } from "./profile/profile-edit/profile-edit.component";
+import {ProfileShowComponent} from "./profile/profile-show/profile-show.component";
+import {ProfileEditComponent} from "./profile/profile-edit/profile-edit.component";
 
-import { PasswordEditComponent } from "./password-edit/password-edit.component";
-import { FacebookComponent } from "./facebook/facebook.component";
-import { PhotoComponent } from "./profile/photo/photo.component";
-import { AssociationHomepageComponent } from "./associations/association-homepage/association-homepage.component";
-import { AssociationSidebarComponent } from "./associations/association-sidebar/association-sidebar.component";
-import { AssociationPageComponent } from "./associations/association-page/association-page.component";
-import { AssociationMembersComponent } from "./associations/association-members/association-members.component";
-import { AssociationSettingsComponent } from "./associations/association-settings/association-settings.component";
-import { MarketplaceHomeComponent } from "./marketplace/marketplace-home/marketplace-home.component";
-import { MarketplaceSidebarComponent } from "./marketplace/marketplace-sidebar/marketplace-sidebar.component";
-import { MarketplaceBasketComponent } from "./marketplace/marketplace-basket/marketplace-basket.component";
-import { MarketplaceHistoryComponent } from "./marketplace/marketplace-history/marketplace-history.component";
-import { PaginationControlsComponent } from "./pagination-controls/pagination-controls.component";
-import { MarketplaceManagerOrdersComponent } from "./marketplace/marketplace-manager-orders/marketplace-manager-orders.component";
-import { MarketplaceManagerCatalogComponent } from "./marketplace/marketplace-manager-catalog/marketplace-manager-catalog.component";
-import { MarketplaceManagerCounterComponent } from "./marketplace/marketplace-manager-counter/marketplace-manager-counter.component";
-import { MarketplaceManagerFundingsComponent } from "./marketplace/marketplace-manager-fundings/marketplace-manager-fundings.component";
-import { BsDropdownModule } from "ngx-bootstrap";
-import { RequestCacheService } from "./request-cache.service";
-import { CachingInterceptor } from "./caching-interceptor";
-import { AuthInterceptor } from "./auth-interceptor";
-import { WidgetsComponent } from "./widgets/widgets.component";
-import { WidgetBirthdaysComponent } from "./widgets/birthdays/birthdays.component";
-import { WidgetPollComponent } from "./widgets/poll/poll.component";
-import { WidgetChatComponent } from "./widgets/chat/chat.component";
-import { ViewForumComponent } from "./forum/view-forum/view-forum.component";
-import { ViewThemeComponent } from "./forum/view-theme/view-theme.component";
-import { ViewTopicComponent } from "./forum/view-topic/view-topic.component";
-import { TimelineComponent } from "./timeline/timeline.component";
-import { LibraryCatalogComponent } from "./library/library-catalog/library-catalog.component";
-import { LibrarySidebarComponent } from "./library/library-sidebar/library-sidebar.component";
-import { LibraryLoansComponent } from "./library/library-loans/library-loans.component";
-import { LibraryManagerCatalogComponent } from "./library/library-manager-catalog/library-manager-catalog.component";
-import { LibraryManagerLoansComponent } from "./library/library-manager-loans/library-manager-loans.component";
+import {PasswordEditComponent} from "./password-edit/password-edit.component";
+import {FacebookComponent} from "./facebook/facebook.component";
+import {PhotoComponent} from "./profile/photo/photo.component";
+import {AssociationHomepageComponent} from "./associations/association-homepage/association-homepage.component";
+import {AssociationSidebarComponent} from "./associations/association-sidebar/association-sidebar.component";
+import {AssociationPageComponent} from "./associations/association-page/association-page.component";
+import {AssociationMembersComponent} from "./associations/association-members/association-members.component";
+import {AssociationSettingsComponent} from "./associations/association-settings/association-settings.component";
+import {MarketplaceHomeComponent} from "./marketplace/marketplace-home/marketplace-home.component";
+import {MarketplaceSidebarComponent} from "./marketplace/marketplace-sidebar/marketplace-sidebar.component";
+import {MarketplaceBasketComponent} from "./marketplace/marketplace-basket/marketplace-basket.component";
+import {MarketplaceHistoryComponent} from "./marketplace/marketplace-history/marketplace-history.component";
+import {PaginationControlsComponent} from "./pagination-controls/pagination-controls.component";
+import {MarketplaceManagerOrdersComponent} from "./marketplace/marketplace-manager-orders/marketplace-manager-orders.component";
+import {MarketplaceManagerCatalogComponent} from "./marketplace/marketplace-manager-catalog/marketplace-manager-catalog.component";
+import {MarketplaceManagerCounterComponent} from "./marketplace/marketplace-manager-counter/marketplace-manager-counter.component";
+import {MarketplaceManagerFundingsComponent} from "./marketplace/marketplace-manager-fundings/marketplace-manager-fundings.component";
+import {BsDropdownModule} from "ngx-bootstrap";
+import {RequestCacheService} from "./request-cache.service";
+import {CachingInterceptor} from "./caching-interceptor";
+import {AuthInterceptor} from "./auth-interceptor";
+import {WidgetsComponent} from "./widgets/widgets.component";
+import {WidgetBirthdaysComponent} from "./widgets/birthdays/birthdays.component";
+import {WidgetPollComponent} from "./widgets/poll/poll.component";
+import {WidgetChatComponent} from "./widgets/chat/chat.component";
+import {ViewForumComponent} from "./forum/view-forum/view-forum.component";
+import {ViewThemeComponent} from "./forum/view-theme/view-theme.component";
+import {ViewTopicComponent} from "./forum/view-topic/view-topic.component";
+import {TimelineComponent} from "./timeline/timeline.component";
+import {LibraryCatalogComponent} from "./library/library-catalog/library-catalog.component";
+import {LibrarySidebarComponent} from "./library/library-sidebar/library-sidebar.component";
+import {LibraryLoansComponent} from "./library/library-loans/library-loans.component";
+import {LibraryManagerCatalogComponent} from "./library/library-manager-catalog/library-manager-catalog.component";
+import {LibraryManagerLoansComponent} from "./library/library-manager-loans/library-manager-loans.component";
+import {DebounceChangeDirective} from "./facebook/debounce-change.directive";
 
 // alternatively if you only need to include a subset of languages
 var hljs: any;
 
 export function highlightJsFactory() {
-  return hljs;
+    return hljs;
 }
 
 registerLocaleData(localeFr);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent,
-    NavbarComponent,
-    ProfileShowComponent,
-    ProfileEditComponent,
-    PasswordEditComponent,
-    FacebookComponent,
-    PhotoComponent,
-    AllAssociationsComponent,
-    AssociationHomepageComponent,
-    AssociationSidebarComponent,
-    AssociationPageComponent,
-    AssociationMembersComponent,
-    AssociationSettingsComponent,
-    MarketplaceHomeComponent,
-    MarketplaceSidebarComponent,
-    MarketplaceBasketComponent,
-    MarketplaceHistoryComponent,
-    PaginationControlsComponent,
-    MarketplaceManagerOrdersComponent,
-    MarketplaceManagerCatalogComponent,
-    MarketplaceManagerCounterComponent,
-    MarketplaceManagerFundingsComponent,
-    WidgetsComponent,
-    WidgetBirthdaysComponent,
-    WidgetChatComponent,
-    WidgetPollComponent,
-    ViewForumComponent,
-    ViewThemeComponent,
-    ViewTopicComponent,
-    TimelineComponent,
-    LibraryCatalogComponent,
-    LibrarySidebarComponent,
-    LibraryLoansComponent,
-    LibraryManagerCatalogComponent,
-    LibraryManagerLoansComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    NgSelectModule,
-    HttpClientModule,
-    BsDropdownModule.forRoot(),
-    AngularMarkdownEditorModule.forRoot({ iconlibrary: "fa" }),
-    MarkdownModule.forRoot({
-      markedOptions: {
-        provide: MarkedOptions,
-        useValue: {
-          breaks: true,
-          gfm: true,
-          // renderer: new Renderer(), Here we can add custom markdown tags
-          sanitize: true
-        }
-      }
-    }),
-    NotifierModule.withConfig({
-      position: {
-        horizontal: {
-          position: 'right',
-          distance: 12
-        },
-        vertical: {
-          position: 'bottom',
-          distance: 12,
-          gap: 10
-        }
-      },
-      theme: 'material',
-      behaviour: {
-        autoHide: 5000,
-        onClick: 'hide',
-        onMouseover: 'pauseAutoHide',
-        showDismissButton: true,
-        stacking: 6
-      },
-      animations: {
-        enabled: true,
-        show: {
-          preset: 'slide',
-          speed: 500,
-          easing: 'ease'
-        },
-        hide: {
-          preset: 'slide',
-          speed: 500,
-          easing: 'ease',
-          offset: 50
-        },
-        shift: {
-          speed: 500,
-          easing: 'ease'
-        },
-        overlap: 250
-      }
-    }),
-    NgxPaginationModule,
-    NgbModule,
-    DragulaModule.forRoot()
-  ],
-  providers: [
-    ApiService,
-    CookieService,
-    RequestCacheService,
-    { provide: HTTP_INTERCEPTORS, useClass: CachingInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    { provide: LOCALE_ID, useValue: "fr" }
-  ],
-  bootstrap: [AppComponent],
-  entryComponents: [
-    WidgetBirthdaysComponent,
-    WidgetChatComponent,
-    WidgetPollComponent
-  ]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+        NavbarComponent,
+        ProfileShowComponent,
+        ProfileEditComponent,
+        PasswordEditComponent,
+        FacebookComponent,
+        PhotoComponent,
+        AllAssociationsComponent,
+        AssociationHomepageComponent,
+        AssociationSidebarComponent,
+        AssociationPageComponent,
+        AssociationMembersComponent,
+        AssociationSettingsComponent,
+        MarketplaceHomeComponent,
+        MarketplaceSidebarComponent,
+        MarketplaceBasketComponent,
+        MarketplaceHistoryComponent,
+        PaginationControlsComponent,
+        MarketplaceManagerOrdersComponent,
+        MarketplaceManagerCatalogComponent,
+        MarketplaceManagerCounterComponent,
+        MarketplaceManagerFundingsComponent,
+        WidgetsComponent,
+        WidgetBirthdaysComponent,
+        WidgetChatComponent,
+        WidgetPollComponent,
+        ViewForumComponent,
+        ViewThemeComponent,
+        ViewTopicComponent,
+        TimelineComponent,
+        LibraryCatalogComponent,
+        LibrarySidebarComponent,
+        LibraryLoansComponent,
+        LibraryManagerCatalogComponent,
+        LibraryManagerLoansComponent,
+        DebounceChangeDirective
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        NgSelectModule,
+        HttpClientModule,
+        BsDropdownModule.forRoot(),
+        AngularMarkdownEditorModule.forRoot({iconlibrary: "fa"}),
+        MarkdownModule.forRoot({
+            markedOptions: {
+                provide: MarkedOptions,
+                useValue: {
+                    breaks: true,
+                    gfm: true,
+                    // renderer: new Renderer(), Here we can add custom markdown tags
+                    sanitize: true
+                }
+            }
+        }),
+        NotifierModule.withConfig({
+            position: {
+                horizontal: {
+                    position: 'right',
+                    distance: 12
+                },
+                vertical: {
+                    position: 'bottom',
+                    distance: 12,
+                    gap: 10
+                }
+            },
+            theme: 'material',
+            behaviour: {
+                autoHide: 5000,
+                onClick: 'hide',
+                onMouseover: 'pauseAutoHide',
+                showDismissButton: true,
+                stacking: 6
+            },
+            animations: {
+                enabled: true,
+                show: {
+                    preset: 'slide',
+                    speed: 500,
+                    easing: 'ease'
+                },
+                hide: {
+                    preset: 'slide',
+                    speed: 500,
+                    easing: 'ease',
+                    offset: 50
+                },
+                shift: {
+                    speed: 500,
+                    easing: 'ease'
+                },
+                overlap: 250
+            }
+        }),
+        NgxPaginationModule,
+        NgbModule,
+        DragulaModule.forRoot()
+    ],
+    providers: [
+        ApiService,
+        CookieService,
+        RequestCacheService,
+        {provide: HTTP_INTERCEPTORS, useClass: CachingInterceptor, multi: true},
+        {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+        {provide: LOCALE_ID, useValue: "fr"}
+    ],
+    bootstrap: [AppComponent],
+    entryComponents: [
+        WidgetBirthdaysComponent,
+        WidgetChatComponent,
+        WidgetPollComponent
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- frontend/src/app/facebook/facebook.component.ts
+++ frontend/src/app/facebook/facebook.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ApiService} from "../api.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {User} from "../models/user";
 
 /**
  * IMPORTANT NOTE
@@ -12,26 +14,63 @@ import {ApiService} from "../api.service";
     templateUrl: './facebook.component.html',
     styleUrls: ['./facebook.component.scss']
 })
-export class FacebookComponent implements OnInit {
+export class FacebookComponent {
 
     $users: any;
     promotions = [];
     p = 0; // The current page
 
+    search_promotion = "";
+    search_text = "";
 
-    constructor(private api: ApiService) {
-    }
-
-    ngOnInit() {
-        this.$users = this.api.getUsers();
+    constructor(private api: ApiService, private activatedRoute: ActivatedRoute, private router: Router) {
+        this.activatedRoute.queryParams.subscribe(params => this.refreshPromotion(params));
         this.api.get("promotions/").subscribe(
             res => {
-                for(let p of res["promotions"]){
+                for (let p of res["promotions"]) {
                     this.promotions.push(p)
                 }
             },
             err => 0
         )
+    }
+
+    refreshPromotion(params) {
+        let url = "users/";
+        let added_one_param = false ;
+
+        if (params["promo"]) {
+            url += added_one_param ? "&" : "?" ; // Ok there is no need to add a condition, be let's be consistant
+            url += "promo=" + params["promo"];
+            added_one_param = true ;
+        }
+
+        if (params["search"]) {
+            url += added_one_param ? "&" : "?" ;
+            url += "search=" + params["search"];
+        }
+
+        this.$users = this.api.get(url);
+    }
+
+    updateParameters() {
+
+        let params = {};
+        let promo = parseInt(this.search_promotion);
+        if(!isNaN(promo)){
+            params["promo"] = promo ;
+        }
+
+        if(this.search_text.length > 0){
+            params["search"] = this.search_text.trim() ;
+        }
+
+        this.router.navigate(
+            [],
+            {
+                relativeTo: this.activatedRoute,
+                queryParams: params
+            });
     }
 
 }
