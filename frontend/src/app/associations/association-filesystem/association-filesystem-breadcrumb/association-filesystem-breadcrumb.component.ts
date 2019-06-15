e8023df41a9ca17ce6dd0915e089c84e8f3f1bfd
Adrien Bocquet <adrienbocquet38@gmail.com>
2019-06-15 18:15:17 +0200

    Ability to move files 2

--- frontend/src/app/associations/association-filesystem/association-filesystem-breadcrumb/association-filesystem-breadcrumb.component.ts
+++ frontend/src/app/associations/association-filesystem/association-filesystem-breadcrumb/association-filesystem-breadcrumb.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 import { ApiService } from "../../../api.service";
 
 @Component({
