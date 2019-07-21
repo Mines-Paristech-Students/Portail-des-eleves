f2d07d8a105a1ec6caddc8a688dc09986e510932
Adrien Bocquet <adrienbocquet38@gmail.com>
2019-07-21 19:02:01 -0400

    Removed migrations

--- bash_scripts/create_and_populate_database.bash
+++ bash_scripts/create_and_populate_database.bash
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 # erases the database, creates a new one and loads fixtures in it
 
+rm -Rf associations/migrations/*
+rm -Rf authentication/migrations/*
+rm -Rf chat/migrations/*
+rm -Rf forum/migrations/*
+rm -Rf polls/migrations/*
+rm -Rf repartitions/migrations/*
+rm -Rf subscriptions/migrations/*
+
+
 python manage.py reset_db --noinput
 python manage.py makemigrations
 python manage.py migrate
+
 python manage.py loaddata authentication
 python manage.py loaddata associations
+python manage.py loaddata chat
 python manage.py loaddata forum
+python manage.py loaddata polls
+python manage.py loaddata repartitions
+python manage.py loaddata subscriptions
