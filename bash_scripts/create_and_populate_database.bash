ea4145b906de4b525899011b783685c9f5357a0a
Adrien Bocquet <adrienbocquet38@gmail.com>
2019-07-21 19:59:37 -0400

    Fixed fixtures

--- bash_scripts/create_and_populate_database.bash
+++ bash_scripts/create_and_populate_database.bash
@@ -11,7 +11,15 @@ rm -Rf subscriptions/migrations/*
 
 
 python manage.py reset_db --noinput
-python manage.py makemigrations
+
+python manage.py makemigrations associations
+python manage.py makemigrations authentication
+python manage.py makemigrations chat
+python manage.py makemigrations forum
+python manage.py makemigrations polls
+python manage.py makemigrations repartitions
+python manage.py makemigrations subscriptions
+
 python manage.py migrate
 
 python manage.py loaddata authentication
