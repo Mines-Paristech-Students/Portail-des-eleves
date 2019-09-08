aad0da5938986cd6b5675ef17258cfae53c39352
Pierre <pierre.wan-fat@mines-paristech.fr>
2019-09-08 13:55:05 +0800

    Modify the library models serializers.
    Modify the LibrarySerializer to create and update Django models.
    Add LoanableShortSerializer.

--- associations/serializers/library.py
+++ associations/serializers/library.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from rest_framework.relations import PrimaryKeyRelatedField
 
-from associations.models import Library, Loanable, Loan
+from associations.models import Association, Library, Loanable, Loan
+from associations.serializers.association import AssociationsShortSerializer
 
 
 class LibraryShortSerializer(serializers.ModelSerializer):
@@ -10,13 +11,20 @@ class LibraryShortSerializer(serializers.ModelSerializer):
         fields = ('id', 'enabled', "association")
 
 
-from associations.serializers.association import AssociationsShortSerializer
+class LoanableShortSerializer(serializers.ModelSerializer):
+    """Used to serialize loanables inside a LibrarySerializer."""
+
+    class Meta:
+        model = Loanable
+        fields = ('name', 'description', 'image', 'comment')
 
 
 class LoanableSerializer(serializers.ModelSerializer):
+    library = serializers.PrimaryKeyRelatedField(many=False, read_only=False, queryset=Library.objects)
+
     class Meta:
         model = Loanable
-        fields = ("id", "name", "description", "image", "comment")
+        fields = ("id", "name", "description", "image", "comment", "library")
 
     def to_representation(self, instance):
         res = super().to_representation(instance)
@@ -51,9 +59,50 @@ class LoanSerializer(serializers.ModelSerializer):
 
 
 class LibrarySerializer(serializers.ModelSerializer):
-    association = AssociationsShortSerializer()
-    loanables = LoanableSerializer(many=True)
+    loanables = LoanableShortSerializer(many=True)
 
     class Meta:
         model = Library
         fields = ("id", "enabled", "association", "loanables")
+
+    def create(self, validated_data):
+        """Create a new instance of Library based upon validated_data."""
+
+        # A new Library is linked to an existing association.
+        association_data = validated_data.pop('association')
+        association = Association.objects.get(pk=association_data)
+
+        # A new Library may come with new loanables.
+        loanables_data = validated_data.pop('loanables')
+
+        # Insert the Library first, then the loanables, because the newly created Library object is needed to create
+        # the loanables.
+        library = Library.objects.create(**validated_data)
+        association.library = library
+        association.save()
+
+        # Insert the loanables.
+        for loanable_data in loanables_data:
+            Loanable.objects.create(name=loanable_data['name'],
+                                    description=loanable_data.get('description', None),
+                                    image=loanable_data.get('image', None),
+                                    comment=loanable_data.get('comment', None),
+                                    library=library)
+
+        return library
+
+    def update(self, instance, validated_data):
+        """
+            Update an existing instance of Library based upon validated_data.\n
+            The nested fields association and loanables will not be updated.
+        """
+
+        instance.enabled = validated_data.get('enabled', instance.enabled)
+        instance.save()
+
+    def to_representation(self, instance):
+        res = super(serializers.ModelSerializer, self).to_representation(instance)
+
+        res['association'] = AssociationsShortSerializer().to_representation(
+            Association.objects.get(pk=res['association']))
+        return res
