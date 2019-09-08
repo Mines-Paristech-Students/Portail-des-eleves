5d6793f9877f73c702495a9e24fc025a3cd4d30e
Pierre <pierre.wan-fat@mines-paristech.fr>
2019-09-08 22:33:31 +0800

    Modify associations.models.library.Loanable and Loan.
    Add Loanable#is_borrowed and the REJECTED status to Loan, as well as
    a nice ASCII diagram to explain the statuses.

--- associations/models/library.py
+++ associations/models/library.py
@@ -21,6 +21,13 @@ class Loanable(models.Model):
     comment = models.TextField(null=True, blank=True)
     library = models.ForeignKey(Library, models.CASCADE, related_name="loanables")
 
+    def is_borrowed(self):
+        for loan in self.loans.all():
+            if loan.status == 'BORROWED':
+                return True
+
+        return False
+
 
 class Loan(models.Model):
     id = models.AutoField(primary_key=True)
@@ -28,15 +35,22 @@ class Loan(models.Model):
     loanable = models.ForeignKey(Loanable, on_delete=models.CASCADE, related_name="loans")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    loan_date = models.DateTimeField(auto_now=True)
+    loan_date = models.DateTimeField(auto_now=False, null=True)
     expected_return_date = models.DateTimeField(auto_now=False, null=True)
     real_return_date = models.DateTimeField(auto_now=False, null=True)
 
+    #           --- CANCELLED
+    #          /
+    # PENDING ----- ACCEPTED ------ BORROWED ----- RETURNED
+    #         \
+    #           --- REJECTED
+
     STATUS = (
-        ("BORROWED", "Emprunté"),  # The product has been given. The order cannot be CANCELLED then
-        ("CANCELLED", "Annulé"),  # The person cancels the order
-        ("PENDING", "En attente"),  # The person has asked for the loan
-        ("REJECTED", "Refusé"),  # The library manager has refused to loan the product
-        ("RETURNED", "Rendu"),  # The loan is finished
+        ("PENDING", "En attente"),  # The user has asked for the loan.
+        ("CANCELLED", "Annulé"),  # The user has cancelled the loan.
+        ("REJECTED", "Refusé"),  # The library administrator has refused the loan.
+        ("ACCEPTED", "Accepté"),  # The library administrator has accepted the loan.
+        ("BORROWED", "Emprunté"),  # The product has been given. The order cannot be CANCELLED anymore.
+        ("RETURNED", "Rendu"),  # The product has been returned.
     )
-    status = models.CharField(choices=STATUS, max_length=200)
+    status = models.CharField(choices=STATUS, max_length=200, default="PENDING")
