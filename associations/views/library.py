5d5f5c712c3f4b9757338f2cf0ab9bac94d70ef5
Piwakk <pierre.wan-fat@mines-paristech.fr>
2019-09-10 18:24:26 +0800

    Edit LoansViewSet to select a different Serializer depending on the action.

--- associations/views/library.py
+++ associations/views/library.py
@@ -1,10 +1,13 @@
+from django.db.models import Q
+
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework import viewsets
 
 from associations.models import Library, Loan, Loanable
 from associations.permissions import CanManageLibrary
-from associations.serializers import LibrarySerializer, LoanSerializer, LoanableSerializer
+from associations.serializers import LibrarySerializer, CreateLoanSerializer, LoanSerializer, UpdateLoanSerializer, \
+    LoanableSerializer
 
 
 class LibraryViewSet(viewsets.ModelViewSet):
@@ -15,12 +18,26 @@ class LibraryViewSet(viewsets.ModelViewSet):
 
 class LoansViewSet(viewsets.ModelViewSet):
     queryset = Loan.objects.all()
-    serializer_class = LoanSerializer
-    permission_classes = (CanManageLibrary,)
+    #permission_classes = (CanManageLibrary,)
 
     filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
-    filter_fields = ('status', 'user', "loan_date", "loanable__library__id")
+    filter_fields = ('status', 'user', 'loan_date', 'loanable__library__id')
     ordering = ('loan_date',)
+
+    def get_serializer_class(self):
+        if self.action in ('create',):
+            return CreateLoanSerializer
+        elif self.action in ('update', 'partial_update'):
+            return UpdateLoanSerializer
+        else:
+            return LoanSerializer
+
+    def get_queryset(self):
+        # The library for which the user is library administrator.
+        libraries = [role.association.library for role in self.request.user.roles.all() if role.library]
+
+        return Loan.objects.filter(Q(user=self.request.user) |
+                                   Q(loanable__library__in=libraries))
 
 
 class LoanableViewSet(viewsets.ModelViewSet):
