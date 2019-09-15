47445e567448e05044ef6d077a0a80fb8851226d
Pierre <pierre.wan-fat@mines-paristech.fr>
2019-09-15 18:27:51 +0800

    Create tests for /users/.

---
+++ authentication/tests/test_profile.py
@@ -0,0 +1,159 @@
+from datetime import date
+
+from authentication.models import User
+from authentication.serializers import UserSerializer
+from backend.tests_utils import BackendTestCase
+
+from rest_framework.renderers import JSONRenderer
+
+
+class ProfileTestCase(BackendTestCase):
+    fixtures = ['test_authentication.yaml']
+
+    def assertContentIsEqualToUser(self, res, user):
+        self.assertEqual(res.content, JSONRenderer().render(UserSerializer(user).data))
+
+    ############
+    # RETRIEVE #
+    ############
+
+    def test_if_not_logged_in_then_cannot_retrieve_user(self):
+        res = self.get('users/17bocquet/')
+        self.assertStatusCode(res, 401)
+
+    def test_if_logged_in_then_can_retrieve_user(self):
+        self.login('17simple')
+        res = self.get('users/17bocquet/')
+        self.assertStatusCode(res, 200)
+        self.assertContentIsEqualToUser(res, User.objects.get(pk='17bocquet'))
+
+    def test_if_logged_in_then_can_retrieve_current_user(self):
+        self.login('17simple')
+        res = self.get('users/current/')
+        self.assertStatusCode(res, 200)
+        self.assertContentIsEqualToUser(res, User.objects.get(pk='17simple'))
+
+    ########
+    # LIST #
+    ########
+
+    def test_if_not_logged_in_then_cannot_list_user(self):
+        res = self.get('users/')
+        self.assertStatusCode(res, 401)
+
+    def test_if_logged_in_then_can_list_user(self):
+        self.login('17simple')
+        res = self.get('users/')
+        self.assertStatusCode(res, 200)
+
+    ##########
+    # CREATE #
+    ##########
+
+    new_user_data = {
+        'id': '17test',
+        'password': 'pbkdf2_sha256$100000$tos6gO0V3tNL$Vd14vNq3N5MwGX6TsvBV0RW+DQzGpy3OGfKqCtL3kls=',
+        'first_name': 'Test',
+        'last_name': 'Test',
+        'email': 'test@mpt.fr',
+        'is_admin': 'false',
+        'promo': '17'
+    }
+
+    def test_if_not_logged_in_then_cannot_create_user(self):
+        res = self.post('users/', self.new_user_data)
+        self.assertStatusCode(res, 401)
+
+    def test_if_not_admin_then_cannot_create_user(self):
+        self.login('17simple')
+        res = self.post('users/', self.new_user_data)
+        self.assertStatusCode(res, 403)
+
+    def test_if_admin_then_can_create_user(self):
+        self.login('17admin')
+        res = self.post('users/', self.new_user_data)
+        self.assertStatusCode(res, 201)
+        self.assertTrue(User.objects.filter(pk='17test').exists())
+
+    ##########
+    # UPDATE #
+    ##########
+
+    limited_edit_user_data = {
+        'nickname': 'Nick',
+        'phone': '123456',
+        'room': '123',
+        'address': '60 boulevard SM',
+        'city_of_origin': 'Paris',
+        'option': 'Mécatro',
+        'sports': 'Pichage',
+        'roommate': ['17bocquet'],
+        'minesparent': ['17wan-fat'],
+    }
+
+    full_edit_user_data = {
+        'first_name': 'Matthieu',
+        'last_name': 'Mazières',
+        'nickname': 'Nick',
+        'birthday': date(1963, 2, 3),
+        'email': 'matmaz@mpt.fr',
+        'promo': 62,
+        'phone': '123456',
+        'room': '123',
+        'address': '60 boulevard SM',
+        'city_of_origin': 'Paris',
+        'option': 'Mécatro',
+        'is_ast': True,
+        'is_isupfere': True,
+        'is_in_gapyear': True,
+        'sports': 'Pichage',
+        'roommate': ['17bocquet'],
+        'minesparent': ['17wan-fat'],
+        'is_active': True,
+        'is_admin': True,
+    }
+
+    def test_if_user_then_can_edit_own_profile_with_limited_data(self):
+        self.login('17simple')
+        res = self.patch('users/17simple/', data=self.limited_edit_user_data)
+        self.assertStatusCode(res, 200)
+
+        user = User.objects.get(pk='17simple')
+        for field in self.limited_edit_user_data:
+            if field in ['roommate', 'minesparent']:
+                self.assertEqual([u.id for u in user.__getattribute__(field).all()], self.limited_edit_user_data[field])
+            else:
+                self.assertEqual(user.__getattribute__(field), self.limited_edit_user_data[field])
+
+    def test_if_not_admin_then_cannot_edit_other_user_profile(self):
+        self.login('17simple')
+        res = self.patch('users/17bocquet/', data=self.limited_edit_user_data)
+        self.assertStatusCode(res, 403)
+
+    def test_if_admin_then_can_edit_other_user_profile(self):
+        self.login('17admin')
+        res = self.patch('users/17simple/', data=self.full_edit_user_data)
+        self.assertStatusCode(res, 200)
+
+        user = User.objects.get(pk='17simple')
+        for field in self.full_edit_user_data:
+            if field in ('roommate', 'minesparent'):
+                self.assertEqual([u.id for u in user.__getattribute__(field).all()], self.full_edit_user_data[field])
+            else:
+                self.assertEqual(user.__getattribute__(field), self.full_edit_user_data[field])
+
+    ##########
+    # DELETE #
+    ##########
+
+    def test_if_not_admin_then_cannot_delete_user(self):
+        self.login('17simple')
+        res = self.delete('users/17simple/')
+        self.assertStatusCode(res, 403)
+        self.assertTrue(User.objects.filter(pk='17simple').exists())
+
+    def test_if_admin_then_can_delete_user(self):
+        self.login('17admin')
+        res = self.delete('users/17simple/')
+        self.assertStatusCode(res, 204)
+        self.assertFalse(User.objects.filter(pk='17simple').exists())
--- backend/tests_utils.py
+++ backend/tests_utils.py
@@ -8,6 +8,8 @@ from authentication.models import User
 class BackendTestCase(APITestCase):
     """Define some helpful functions for testing the backend applications."""
 
+    api_base = '/api/v1/'
+
     def create_user(self, id='user', admin=False):
         """
         Create an user, with password='password'
@@ -61,3 +63,61 @@ class BackendTestCase(APITestCase):
         user = self.create_user(id, admin)
         self.login(id, 'password')
         return user
+
+    def assertStatusCode(self, res, status_code, user_msg=''):
+        msg = ''
+        if hasattr(res, 'url'):
+            msg += f'URL: {res.url}\n'
+        if hasattr(res, 'content'):
+            msg += f'Content: {res.content}\n'
+
+        msg += f'\n{user_msg}'
+
+        self.assertEqual(res.status_code, status_code, msg=msg)
+
+    def assertStatusCodeIn(self, res, status_codes, user_msg=''):
+        msg = ''
+        if hasattr(res, 'url'):
+            msg += f'URL: {res.url}\n'
+        if hasattr(res, 'content'):
+            msg += f'Content: {res.content}\n'
+
+        msg += f'\n{user_msg}'
+
+        self.assertIn(res.status_code, status_codes, msg=msg)
+
+    def logout(self):
+        """Log the current user out."""
+        self.client.logout()
+
+    def login(self, username, password='password'):
+        """
+        Log an user in.
+        :param str username: the user's username.
+        :param str password: the user's password (default: password, should not change)
+        :return: the response from token_obtain_pair.
+        """
+
+        self.logout()
+
+        url = reverse('token_obtain_pair')
+        data = {'id': username, 'password': password}
+        return self.client.post(url, data, format='json')
+
+    def get(self, url, data=None):
+        return self.client.get(self.api_base + url, data)
+
+    def post(self, url, data=None, format='json'):
+        return self.client.post(self.api_base + url, data, format=format)
+
+    def patch(self, url, data=None, format='json'):
+        return self.client.patch(self.api_base + url, data, format=format)
+
+    def delete(self, url, data='', format=None):
+        return self.client.delete(self.api_base + url)
+
+    def head(self, url, data=None):
+        return self.client.head(self.api_base + url, data)
+
+    def options(self, url, data=None):
+        return self.client.options(self.api_base + url, data)
