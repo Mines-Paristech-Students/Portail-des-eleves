2dbc0c3de109c6d9c41fc740a34855836cbda20a
Piwakk <pierre.wan-fat@mines-paristech.fr>
2019-09-18 15:18:36 +0800

    Add tests for the page module.

--- associations/models/page.py
+++ associations/models/page.py
@@ -2,9 +2,11 @@ from django.db import models
 
 from associations.models.association import Association
 
+
 class Page(models.Model):
     id = models.AutoField(primary_key=True)
 
-    title = models.CharField(max_length=200)
     association = models.ForeignKey(Association, on_delete=models.CASCADE, related_name="pages")
-    text = models.TextField(blank=True, null=True, default=None)
+
+    title = models.CharField(max_length=200)
+    text = models.TextField(blank=True, default='')
