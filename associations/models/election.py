8d630585ba86307678714785be790d0a104695f6
Piwakk <pierre.wan-fat@mines-paristech.fr>
2019-09-18 17:29:14 +0800

    Change fixtures and admin panel to take the models changes into account.

---
+++ associations/models/election.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.utils.functional import cached_property
+
+from associations.models.association import Association
+from authentication.models import User
+
+
+class Election(models.Model):
+    """
+        The Election class allows to carry a vote. It should always guarantee the anonymity of the voter.
+    """
+
+    id = models.AutoField(primary_key=True)
+
+    association = models.ForeignKey(Association, on_delete=models.CASCADE, related_name='elections')
+
+    registered_voters = models.ManyToManyField(User,
+                                               related_name='allowed_elections',
+                                               help_text='User who are allowed to vote to this election.')
+    voters = models.ManyToManyField(User,
+                                    related_name='voted_elections',
+                                    help_text='People who have already voted to this election.')
+
+    starts_at = models.DateTimeField(null=False)
+    ends_at = models.DateTimeField(null=False)
+
+    max_choices_per_vote = models.PositiveIntegerField(default=1,
+                                                       help_text='The number of choices allowed per vote.')
+
+
+class VoteChoice(models.Model):
+    """
+        Represents a choice of an election.
+    """
+
+    id = models.AutoField(primary_key=True)
+
+    election = models.ForeignKey(Election, on_delete=models.CASCADE, related_name='choices')
+    name = models.CharField(max_length=200)
+
+
+class Vote(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    election = models.ForeignKey(Election, on_delete=models.CASCADE, related_name='votes')
+    choices = models.ManyToManyField(VoteChoice, related_name='votes')
+
+    @cached_property
+    def is_valid(self):  # Should always be true
+        return len(self.choices.count()) <= self.election.max_choices_per_vote
---
+++ associations/views/election.py
@@ -0,0 +1,4 @@
+
+"""
+    /associations/bde/votes/
+"""
