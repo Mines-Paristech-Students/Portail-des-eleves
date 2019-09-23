46eacd78595fc8f47ea640b0fe2ddce53dd08e27
Piwakk <pierre.wan-fat@mines-paristech.fr>
2019-09-23 17:35:00 +0800

    Remove smart_selects_auth

--- backend/urls.py
+++ backend/urls.py
@@ -25,6 +25,4 @@ from backend import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(api_urls)),
-    # Uses the smart_selects library for Chained fields but enforces authentication
-    url(r'^chaining/', include('smart_selects_auth.urls')),
 ] + staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
