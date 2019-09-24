e88105cb09dad4ea3d0cdd9f4430c30fa33a479e
Adrien Bocquet <adrienbocquet38@gmail.com>
2019-09-23 22:22:43 -0400

    [Tag] Pass more tests

--- tags/permissions.py
+++ tags/permissions.py
@@ -1,7 +1,18 @@
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import SuspiciousOperation
 from rest_framework import permissions
 
-from associations.models import Association, Role, Product, Page, Loanable, Loan, Folder, File, Event, Choice
+from associations.models import (
+    Association,
+    Role,
+    Product,
+    Page,
+    Loanable,
+    Loan,
+    Folder,
+    File,
+    Event,
+    Choice,
+)
 from associations.permissions.base_permissions import _get_role_for_user
 from forum.models import Theme, MessageForum, Topic
 from tags.models import Namespace, Tag
@@ -22,7 +33,6 @@ def get_parent_object(obj):
         Page: lambda x: x.association,
         Product: lambda x: x.marketplace.association,
         Role: lambda x: x.association,
-
         # Forum
         Theme: lambda x: x,
         Topic: lambda x: x.theme,
@@ -63,25 +73,34 @@ def can_manage_links_for(user, instance):
 
 
 class NamespacePermission(permissions.BasePermission):
-
     def has_permission(self, request, view):
         if request.method == "POST":
             scope = request.data.get("scope")
             scoped_to = request.data.get("scoped_to")
-            instance = Tag.LINKS[scope].get(pk=scoped_to)
+
+            if scope != "global" and (scope is None or scoped_to is None):
+                raise SuspiciousOperation()
+
+            if scope == "global":
+                return request.user.is_admin
+
+            instance = Tag.LINKS[scope].objects.get(pk=scoped_to)
             return can_manage_tags_for(request.user, instance)
 
         return True
 
     def has_object_permission(self, request, view, namespace):
-        instance = Namespace.SCOPES[namespace.scope].objects.get(namespace.scoped_to)
+        if namespace.scope == "global":
+            return request.user.is_admin
+
+        instance = Namespace.SCOPES[namespace.scope].objects.get(pk=namespace.scoped_to)
         return can_manage_tags_for(request.user, instance)
 
 
 class ManageTagPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == "POST":
-            namespace = Namespace.objects.get(pk=request.data.get('namespace'))
+            namespace = Namespace.objects.get(pk=request.data.get("namespace"))
             scope = Namespace.SCOPES[namespace.scope]
             if scope:
                 instance = scope.objects.get(pk=namespace.scoped_to)
@@ -95,5 +114,8 @@ class ManageTagPermission(permissions.BasePermission):
         if Namespace.SCOPES[namespace.scope] is None:
             return request.user.is_admin
         else:
-            instance = Namespace.SCOPES[namespace.scope].objects.get(pk=namespace.scoped_to)
+            print(namespace)
+            instance = Namespace.SCOPES[namespace.scope].objects.get(
+                pk=namespace.scoped_to
+            )
             return can_manage_tags_for(request.user, instance)
--- tags/tests/namespaces.py
+++ tags/tests/namespaces.py
@@ -2,24 +2,18 @@ from associations.tests.base_test import BaseTestCase
 
 
 class TagNamespaceTestCase(BaseTestCase):
-    fixtures = ['test_authentication.yaml', 'test_marketplace.yaml']
+    fixtures = ["test_authentication.yaml", "test_marketplace.yaml"]
 
     def test_create_scoped_namespace(self):
         ##############################
         # Creation of global namespace
 
         self.login("17admin")
-        res = self.post('/tags/namespaces/', {
-            'scope': "global",
-            'name': 'users'
-        })
+        res = self.post("/tags/namespaces/", {"scope": "global", "name": "users"})
         self.assertStatusCode(res, 201)
 
         # Check cannot create two global namespaces with the same name
-        res = self.post('/tags/namespaces/', {
-            'scope': "global",
-            'name': 'users'
-        })
+        res = self.post("/tags/namespaces/", {"scope": "global", "name": "users"})
 
         self.assertStatusCode(res, 400)
 
@@ -27,119 +21,105 @@ class TagNamespaceTestCase(BaseTestCase):
         # Creation of a namespace for an association
 
         self.login("17admin_pdm")
-        res = self.post('/tags/namespaces/', {
-            'scope': "association",
-            'scoped_to': 'pdm',
-            'name': 'farine'
-        })
+        res = self.post(
+            "/tags/namespaces/",
+            {"scope": "association", "scoped_to": "pdm", "name": "farine"},
+        )
         self.assertStatusCode(res, 201)
 
         # Try to get all namespaces
-        res = self.get('/tags/namespaces/')
+        res = self.get("/tags/namespaces/")
         self.assertStatusCode(res, 200)
-        self.assertJSONEqual(res.content, [
-            {"id": 1, "name": "users", "scope": "global"},
-            {"id": 2, "name": "farine", "scope": "association", "scoped_to": "pdm"}
-        ])
+        self.assertJSONEqual(
+            res.content,
+            [
+                {"id": 1, "name": "users", "scope": "global"},
+                {"id": 2, "name": "farine", "scope": "association", "scoped_to": "pdm"},
+            ],
+        )
 
         # Try to get only the namespaces for the association
-        res = self.get('/tags/namespaces/?scope=association&scoped_to=pdm')
+        res = self.get("/tags/namespaces/?scope=association&scoped_to=pdm")
         self.assertStatusCode(res, 200)
-        self.assertJSONEqual(res.content, [
-            {"id": 2, "name": "farine", "scope": "association", "scoped_to": "pdm"}
-        ])
+        self.assertJSONEqual(
+            res.content,
+            [{"id": 2, "name": "farine", "scope": "association", "scoped_to": "pdm"}],
+        )
 
         # Try to get namespace forgetting the scope
-        res = self.post('/tags/namespaces/', {
-            'scope': 'association',
-            'name': 'test_v'
-        })
+        res = self.post("/tags/namespaces/", {"scope": "association", "name": "test_v"})
         self.assertStatusCode(res, 400)
 
-        res = self.post('/tags/namespaces/', {
-            "scoped_to": 'pdm',
-            'name': 'test_w'
-        })
+        res = self.post("/tags/namespaces/", {"scoped_to": "pdm", "name": "test_w"})
         self.assertStatusCode(res, 400)
 
         # Try to get global namespace
-        res = self.get('/tags/namespaces/?scope=global')
+        res = self.get("/tags/namespaces/?scope=global")
         self.assertStatusCode(res, 200)
-        self.assertJSONEqual(res.content, [
-            {"id": 1, "name": "users", "scope": "global"},
-        ])
+        self.assertJSONEqual(
+            res.content, [{"id": 1, "name": "users", "scope": "global"}]
+        )
 
         #######################################
         # Creation without proper authorization
         self.login("17admin_biero")
-        res = self.post('/tags/namespaces/', {
-            'scope': "association",
-            'scoped_to': 'pdm',
-            'name': 'test_x'
-        })
+        res = self.post(
+            "/tags/namespaces/",
+            {"scope": "association", "scoped_to": "pdm", "name": "test_x"},
+        )
         self.assertStatusCode(res, 403)
 
         self.login("17admin_biero")
-        res = self.post('/tags/namespaces/', {
-            'scope': "global",
-            'name': 'test_y'
-        })
+        res = self.post("/tags/namespaces/", {"scope": "global", "name": "test_y"})
         self.assertStatusCode(res, 403)
 
     def test_edit_namespace(self):
         ###########################
         # Create needed namespaces
         self.login("17admin")
-        res = self.post('/tags/namespaces/', {
-            'scope': "global",
-            'name': 'users'
-        })
+        res = self.post("/tags/namespaces/", {"scope": "global", "name": "users"})
         self.assertStatusCode(res, 201)
 
         self.login("17admin_pdm")
-        res = self.post('/tags/namespaces/', {
-            'scope': "association",
-            'scoped_to': 'pdm',
-            'name': 'farine'
-        })
+        res = self.post(
+            "/tags/namespaces/",
+            {"scope": "association", "scoped_to": "pdm", "name": "farine"},
+        )
         self.assertStatusCode(res, 201)
 
         ###########################
         # Basic namespace edition
 
-        self.login('17admin_biero')
+        self.login("17admin_biero")
         # Global namespace
-        res = self.patch('/tags/namespaces/1/', {'name': 'coucou'})
+        res = self.patch("/tags/namespaces/1/", {"name": "coucou"})
         self.assertStatusCode(res, 403)
 
-        res = self.delete('/tags/namespaces/1/')
+        res = self.delete("/tags/namespaces/1/")
         self.assertStatusCode(res, 403)
 
         # Specific namespace that doesn't belong to us
-        res = self.patch('/tags/namespaces/2/', {'name': 'coucou'})
+        res = self.patch("/tags/namespaces/2/", {"name": "coucou"})
         self.assertStatusCode(res, 403)
 
-        res = self.delete('/tags/namespaces/2/')
+        res = self.delete("/tags/namespaces/2/")
         self.assertStatusCode(res, 403)
 
         self.login("17admin")
-        res = self.patch('/tags/namespaces/1/', {'name': 'coucou'})
+        res = self.patch("/tags/namespaces/1/", {"name": "coucou"})
         self.assertStatusCode(res, 200)
 
         self.login("17admin_pdm")
-        res = self.patch('/tags/namespaces/2/', {'name': 'coucou'})
+        res = self.patch("/tags/namespaces/2/", {"name": "coucou"})
         self.assertStatusCode(res, 200)
 
         ############################
         # Try to move the namespace
         self.login("17admin_pdm")
-        res = self.patch('/tags/namespaces/1/', {
-            'scope': 'global'
-        })
+        res = self.patch("/tags/namespaces/1/", {"scope": "global"})
         self.assertStatusCode(res, 400)
 
-        res = self.patch('/tags/namespaces/1/', {
-            'scope': 'association',
-            'scoped_to': 'biero'
-        })
+        res = self.patch(
+            "/tags/namespaces/1/", {"scope": "association", "scoped_to": "biero"}
+        )
         self.assertStatusCode(res, 400)
