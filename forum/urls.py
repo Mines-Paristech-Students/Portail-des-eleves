782e28384fce4a506981de83dc2a489b69245ff0
Adrien Bocquet <adrienbocquet38@gmail.com>
2019-10-27 11:58:12 -0400

    black

--- forum/urls.py
+++ forum/urls.py
@@ -2,15 +2,20 @@ from django.urls import path
 
 from rest_framework_bulk.routes import BulkRouter
 
-from forum.views import ThemeViewSet, TopicViewSet, MessageForumViewSet, NewVoteMessageView
+from forum.views import (
+    ThemeViewSet,
+    TopicViewSet,
+    MessageForumViewSet,
+    NewVoteMessageView,
+)
 
 
 router = BulkRouter()
 
-router.register(r'', ThemeViewSet)
-router.register(r'theme', TopicViewSet)
-router.register(r'topic', MessageForumViewSet)
+router.register(r"", ThemeViewSet)
+router.register(r"theme", TopicViewSet)
+router.register(r"topic", MessageForumViewSet)
 
 urlpatterns = [
-    path('message-vote/', NewVoteMessageView.as_view(), name='new-vote-message'),
+    path("message-vote/", NewVoteMessageView.as_view(), name="new-vote-message")
 ] + router.urls
