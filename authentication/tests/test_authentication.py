a0f3a4a5601659618b88fb28decdd0937ad84f59
Pierre <pierre.wan-fat@mines-paristech.fr>
2019-10-30 23:58:12 +0800

    Update the fake tokens used for the tests.

--- authentication/tests/test_authentication.py
+++ authentication/tests/test_authentication.py
@@ -14,12 +14,15 @@ class AuthenticationTestCase(BaseTestCase):
     # The audience of these tokens is `portail` (it was of course edited in INVALID_AUDIENCE_TOKEN).
     # Except for `VALID_17UNKNOWN_TOKEN`, the user of these tokens is `17simple`.
 
-    INVALID_SIGNATURE_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjE1NzEwMDMwNjAuMjM4ODQxLCJpc3MiOiJzc29fc2VydmVyIiwiYXVkIjoicG9ydGFpbCIsImp0aSI6IjVjY2FjMTg2MmJjODRiZmM5NTc0NTgwZTJjNGY2ZjE2IiwidXNlciI6IjE3c2ltcGxlIn0.Eeum3p-UTy_rVQOGuCknmWwKb8vqlX4j72ldn3wsO87kOrVqYD_ywAp5QVeaIqzTxP2-9VHMmUUncOSlaYocPG8mN4bOSrrmpc_BmNB9UuNw0T87YDk5N2ZhQmt_xvacCx9zYqy5Z_br_JwEq3rblzglC9o51vp0KQqohrEDaUlCJkL5xhGQunT-Dt1-3Xf5CUj8KNzwM2XxUD-RwJFrsTAGDgXYtDojwXDvt8vJZrvq5sEi1woCgHrftZxfun0gXudQ0ZjcCSF05hQOYaAUX4EDgLo3LjOfUylR9BI6D-7qBC2rSXVly4P3t0ewgie3I3ZciTstoPkAEDdCcq7ZZA"
-    EXPIRED_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjE1Njk1MzQyNjAuNTM4NTIxLCJpc3MiOiJzc29fc2VydmVyIiwiYXVkIjoicG9ydGFpbCIsImp0aSI6IjVjY2FjMTg2MmJjODRiZmM5NTc0NTgwZTJjNGY2ZjE2IiwidXNlciI6IjE3c2ltcGxlIn0.WCgXyiorv6nYN135ENwz_N9w7xkTpWuIr_Hx5xxPNs3wNU7ni7VdfQ61bP1TcFbSkyAkpmnJUVEXPyfd8SEroRxKbtszoVc5JWZ7LP12en1qrkWG1goszxzZoA2WGzHoQFOZG5brW4YL97miT_Xzdin-s2m2Y41d4R2UJ107B3uu5Gx-aguXtfMdZ9lPPOrYf-QpcVp8erMrbE2sAL5WWBjfbzPaa8NFSNTveI5wSkpHRvV5QKv6aDAF4zKxzwr3F7u6UTcTa8yOwHAcsVA6ZZWnjALsg3mUDL8wzE4MyydW27BkRlA3kpwcFtqi-HhVckTYVLIlpiKbIdKlMht6_Q"
-    INVALID_ISSUER_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjE1NzEwMDMwNjAuNTQxNTEsImlzcyI6InBpY2hlIiwiYXVkIjoicG9ydGFpbCIsImp0aSI6IjVjY2FjMTg2MmJjODRiZmM5NTc0NTgwZTJjNGY2ZjE2IiwidXNlciI6IjE3c2ltcGxlIn0.VWhqK7ZpnJFtTCevlxE5_potHNhOIF3Gj_z-LK67XMVhUbZm5gP3gR33sNie-PJKEw9QIor4-hxI9goPq24NiHLsG9SiyLde5SGEC1dGD3UR225ikTIOIoUldzCc3tlD8MW6kAXVYEyh9idMfiZ63RWGPuqbjl-b6zqKy19BALcR2I9WMH0-u6DrOwilIzAwCQ1CZFCCPTLhrPoKoz3U3dMmQllXlZUJHvK4wrf8P7cpogMby86jvtIezn5Xoo_wzStfIWIX64KK9lqnrrJZj-03L8aU6d6VYFbpUvsDYbXOU9VeGCfwTlsjI8uOuHNmB4sr1MznPkhuWFSI8K4U_g"
-    INVALID_AUDIENCE_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjE1NzEwMDMwNjAuNTQ0NTU5LCJpc3MiOiJzc29fc2VydmVyIiwiYXVkIjoicG9ydGFpQSIsImp0aSI6IjVjY2FjMTg2MmJjODRiZmM5NTc0NTgwZTJjNGY2ZjE2IiwidXNlciI6IjE3c2ltcGxlIn0.dC1qZMIrimzApihsztyiAFNwILqLumjJF_ra234WFbuWY2T83t5TD89qZcXGD83kfRRUhM10jQqW9LdLcQUHzHVGFNsbH5d70sr1T7WwMRJQ3gMZtRJdhz9lFyiSZUlH01tyoCPgn8pkPeHs1yLKWrfspqvl8Mix5z-eLP7Xo1HFr_7AFeZ0iH3gzYOcnHoSmtlz6bQd2GysWPRMLN1oYiyULpRLH0x0_P8_2Zu13ZSFC2kdx6ijhvc0iq1eOKjuy6OCLmIE3erSeUABC-ISZK9EikHuSRKKwhCiJPwxWvV6EONkysVbcOQkp_aTT0EDvwOAMiqIXZhGF_Lhnywy5Q"
-    VALID_17SIMPLE_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjE1NzEwMDMwNjAuNTYyODA1LCJpc3MiOiJzc29fc2VydmVyIiwiYXVkIjoicG9ydGFpbCIsImp0aSI6IjVjY2FjMTg2MmJjODRiZmM5NTc0NTgwZTJjNGY2ZjE2IiwidXNlciI6IjE3c2ltcGxlIn0.gG6rB5Wb-1Ehas6Jvm83oFMH1WZY3fKio00wywnSWHjtmx2gYD6HQ6sLNs-Mj8WYl3LylDF2CRNFXh9gOhnkgL2wgbppkAfrfRqcJeC9X875Ssv49BtCX8rdcz2Qy98i_xZv1yF-_vVls3W5hP_8K7wFNsrsPX5pcFXYI7TBcKaiLpaeIEUT_zfud6CdiR9K6GWXVSGmCHj9x2UbfDQbeX3LoSrD6Ud0PlVtMXSm4eyH7eCaleV-FKowZkZ8C63SuFeVF9guJ8KSbBkrsQvzSBQ6Hw_gDLZYcO2sR3dTf1j0ebMW50k3SgSbcyg15gq1aO0cNiwO73Jo2POsqHg6Aw"
-    VALID_17UNKNOWN_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjE1NzEwMDQzODYuNjY1Njg3LCJpc3MiOiJzc29fc2VydmVyIiwiYXVkIjoicG9ydGFpbCIsImp0aSI6IjVjY2FjMTg2MmJjODRiZmM5NTc0NTgwZTJjNGY2ZjE2IiwidXNlciI6IjE3dW5rbm93biJ9.a-ojYLWfVfPf1VdZN6WPWC-ADGKtY9WaifFYUX2HxrrkI0GqPujeld-vpeEAseEvArEu2zw0ElrD5x7g8a7SsWXpIn-e86Nz8z-eGY-5toig9bsuR2PkutW0Ql0GDfki-YjN9w3U3QH4ZDqFbGYXdJiYLeU-jCEmrno80reYa7vMLnsWAQvQsoMQFQORPcRCHKDn8hKm_tDHIp6EztWiMroaQ364QHGL7NfJFzdeS6VH-owLhBFQpk_YuSP5xhrRObLuNh-_GO18TTYtx_tHSzTTepyluUHdsIDkTAD8WopK6-ytls5gT3tgswKiBz4O6Vd1i1Q67ULij-hW-6YFsA"
+    # tokens generated with `sso_server.tests.generate_fake_tokens("portail", "17simple")` in the `sso` project.
+    tokens = {
+        "INVALID_SIGNATURE_TOKEN": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjQ3MjYwMjE5ODQuNzk3MzMzLCJpc3MiOiJzc29fc2VydmVyIiwiYXVkIjoicG9ydGFpbCIsImp0aSI6ImJjOTM4Yzc1NjE4NTRiZTU4MTc4YzZiNzVmNWZjNWNkIiwidXNlciI6IjE3c2ltcGxlIn0.Xu4m9AA3R_y_7RN_7v3rrNq9NQ6f0aeOCTbvQp-HZBR6PhQamhSyAj9vZD05oTLl-ManzDvIXjg8QbKZh3-N0HoO5yWUHtM0CKWJUQ07NptH_VldOtvbc8Fb4Lhzr03ScxtQslaUc6DXraWd3GurDCUGRzw9NT7_VybycofwucjvQjE1gFbjqN7TnxAdb8Tye5w-89ge-1FGZvsqgZyvupScdEe7B15QUe2UTm-RsDDV-RG91B6TBoTduFpTzp3oqzrkwONurKjK929Z-Ndq6AHOP17Cj_SL4R6rxlpVsQ50j0LMODhxwBfIHnQsARiLdwGcThl6LgZLYLfciAAAAA",
+        "EXPIRED_TOKEN": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjE1NzE1NTc5ODQuODY2Mjc2LCJpc3MiOiJzc29fc2VydmVyIiwiYXVkIjoicG9ydGFpbCIsImp0aSI6IjI1NmVkOWM5Mjc3NTQwMDU4MTliOTZkZDkxN2U5NmE5IiwidXNlciI6IjE3c2ltcGxlIn0.CaL4yPQVoV3xxG5mUtPMqS-Qf7wApwOtuFwQSWKqU95-S_AhRwncjn0iLKmrxhQKXSBvDEs5BpReM1EiKQYFVcrDj1g7ajjVL9kvJDnhfg77VL7RnqJjyvtdnctIyK6sZFx_tg1wQcxeXkyvFJs0beR8SkDeZT9cb7G_HERh9CxU_z8KsGSGwOhEf0fotBDDBn-04GCBDCUPsayOhfCfbxSv6ZnbcEthPrv4q6panAm29TlbL9X6rerKScblVyooWg4o-9P0o2tdbNF2HyM1-GnLrUbVC3wsGD-Qo-6MdMYsU6bdrD7WRLaYu_YmKxdH4eT6AIgiqCHnkvWrx4u6FA",
+        "INVALID_ISSUER_TOKEN": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjQ3MjYwMjE5ODQuODY2Mjc2LCJpc3MiOiJwaWNoZSIsImF1ZCI6InBvcnRhaWwiLCJqdGkiOiJlYzdiN2IwYWM0ZGY0YmRjODE5ZDNiNmRmYTNjZjhjYyIsInVzZXIiOiIxN3NpbXBsZSJ9.gA1u-m8b-uZlmhYrBdqo1vx1Ggic9XTN-uYiUNbSKuvrsKknE9Gn1JY24DRZjL3Xz_2zoZBAtUxwOYfJvy9GplHEsgSV0_ARDzZ4cAvWqtDpC8Eq6YusZtlN2O9PPStYYgjrgySw0bqEmUa3Vpo9PSPKEggyPq6UO-NkGbAHpLswxc8_jCwqD7795kYHZ0qIw7UKQ_DZAO_C-xnv2hWYlhUHrpk_Hy6Uteoe908ZtVAhB24ZSRor-VsXfP6UepiZ7Ehc20ggm7-mI4yyIbrA865jZldW7CqjRwYyMVoe_dbIBJ2xyzllc2gS_OfwdjcuGQRNVXcToxgvv8cv5v5IWg",
+        "INVALID_AUDIENCE_TOKEN": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjQ3MjYwMjE5ODQuODY2Mjc2LCJpc3MiOiJzc29fc2VydmVyIiwiYXVkIjoicGljaGUiLCJqdGkiOiI4NGI1NTQzZGE2MDI0ZjU2OGE3YWEwNDJlNWJhYTZkNyIsInVzZXIiOiIxN3NpbXBsZSJ9.M7UpJQ9RwrYSsENFw_iN-ODzBPm3x90GTbjrgnGKvTxexyaA-P_KwE1cHCNdVtWqjQam-Zq88xeD_31jE1Fp7LANsQkA9ACfVuDGsOSrEcCn7yaRRJTkwxShyykI6HnNJS2MznRktOgRYUN-usJj30kNS1p7_ZQnsCYRbWt_frwztdPwlnOishTsipOBTLnmk_h9mUlS9YRNb6gsCw8FaPW3BCu7K9O2YbldpGBhq1iqPx26t2W8VeE6l7IT0soNz-KS0xGxwImuzlDQMkFxxRnnzwpyqDfXoleaZO7iTSCRNqzcae9i3fskMqeLbFrrkIlUmUZjTWz2sA-pNHDjfA",
+        "VALID_PICHE_TOKEN": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjQ3MjYwMjE5ODQuODY2Mjc2LCJpc3MiOiJzc29fc2VydmVyIiwiYXVkIjoicG9ydGFpbCIsImp0aSI6IjE4NGM2OTJmNDBhMzQ4M2U5NGY1MDY3MzUzODBhYzIzIiwidXNlciI6InBpY2hlIn0.bNT2NkYYyrrqk551UHQd1HE02T9MRNNsc0M1Gr9bXHQv3bQBhrdTa7ILlcHf4U-rXiVKCh9QpsQZ379-jEUMFWVqSmse17z156Sdk4vQsVGILk660vOwUV_ugyQIXZCbARNTP4FhpXn4GmNMy15zOF2qn1xUw-lPgkEZnxeIuT_eDzFH_8lKjkIthdhBn7jHGiHJ-Kcu2GpOUZd6N44hD3nlAuEgVYRIN4plNFsLq6p-44eXZraF0JebjJa4J0dj-TiQ3e7XsFDjWuwd4K-bkhZF1KfeDQcFMqT7coZIqaC1o88-zQP0n0UYMtjoWVi56wT5Ams3qG_cg_6mOEFrsQ",
+        "VALID_17SIMPLE_TOKEN": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjQ3MjYwMjE5ODQuODY2Mjc2LCJpc3MiOiJzc29fc2VydmVyIiwiYXVkIjoicG9ydGFpbCIsImp0aSI6IjJlOTEwN2ZkODc3YjRhOTA4NzdkZGI1Njc4MjczZTc1IiwidXNlciI6IjE3c2ltcGxlIn0.SaCYMt6M-KtuZQdSsKfJN9v7REpeSKJk9noFxZC5A43dyaQYuk-yiakCH_tOGmD45lvvO3mxUd9AgmvSVrDoAhU_ybujWRV30stF00j8tTaD70zncISBLM3vFCTDWpcq1JX6_iXRZsYYA_JsasY-dDwBURVS9b-YvtZj4vFSA3OF87KUmce8PNEJ9MZiuei7NkIYlKhgYXO24Y0D3HhqrFj2DD2okajU2BCsmuCfhd6lJXKsu5l4TG8jNpfc4ao6mmCtJVSOOMtpMgLts6Lg3q9rZqAF2UIYyCgrQ7OajQ2GkkurHGv_in-5FbaLiwI36Cq2XYFOlVsMgwvfw9GuLg",
+    }
 
     def login(self, token: str = ""):
         return self.get(f"/auth/login/?access={token}")
@@ -50,7 +53,7 @@ class AuthenticationTestCase(BaseTestCase):
         )
 
     def test_if_invalid_signature_token_then_cannot_login(self):
-        res = self.login(self.INVALID_SIGNATURE_TOKEN)
+        res = self.login(self.tokens["INVALID_SIGNATURE_TOKEN"])
         self.assertStatusCodeIn(res, [401, 403])
         self.assertEqual(
             res.data["detail"],
@@ -59,25 +62,25 @@ class AuthenticationTestCase(BaseTestCase):
         )
 
     def test_if_expired_token_then_cannot_login(self):
-        res = self.login(self.EXPIRED_TOKEN)
+        res = self.login(self.tokens["EXPIRED_TOKEN"])
         self.assertStatusCodeIn(res, [401, 403])
         self.assertEqual(res.data["detail"], "Expired token.", msg=res.data["detail"])
 
     def test_if_invalid_issuer_token_then_cannot_login(self):
-        res = self.login(self.INVALID_ISSUER_TOKEN)
+        res = self.login(self.tokens["INVALID_ISSUER_TOKEN"])
         self.assertStatusCodeIn(res, [401, 403])
         self.assertEqual(res.data["detail"], "Invalid issuer.", msg=res.data["detail"])
 
     def test_if_invalid_audience_token_then_cannot_login(self):
-        res = self.login(self.INVALID_AUDIENCE_TOKEN)
+        res = self.login(self.tokens["INVALID_AUDIENCE_TOKEN"])
         self.assertStatusCodeIn(res, [401, 403])
         self.assertEqual(
             res.data["detail"], "Invalid audience.", msg=res.data["detail"]
         )
 
     def test_if_invalid_user_token_then_not_authenticated(self):
-        self.assertFalse(User.objects.filter(pk="17unknown").exists())
-        res = self.login(self.VALID_17UNKNOWN_TOKEN)
+        self.assertFalse(User.objects.filter(pk="piche").exists())
+        res = self.login(self.tokens["VALID_PICHE_TOKEN"])
         self.assertStatusCode(
             res, 200
         )  # The user verification is done at the next step, so the cookie is set.
@@ -85,7 +88,7 @@ class AuthenticationTestCase(BaseTestCase):
 
     def test_if_valid_user_token_then_authenticated(self):
         self.assertTrue(User.objects.filter(pk="17simple").exists())
-        res = self.login(self.VALID_17SIMPLE_TOKEN)
+        res = self.login(self.tokens["VALID_17SIMPLE_TOKEN"])
         self.assertStatusCode(res, 200)
         self.assertIsAuthenticated("17simple")
 
@@ -95,7 +98,7 @@ class AuthenticationTestCase(BaseTestCase):
 
     def test_logout(self):
         self.assertTrue(User.objects.filter(pk="17simple").exists())
-        res = self.login(self.VALID_17SIMPLE_TOKEN)
+        res = self.login(self.tokens["VALID_17SIMPLE_TOKEN"])
         self.assertStatusCode(res, 200)
         self.assertIsAuthenticated("17simple")
 
