592e4eac1dfe8dfe26e06a1b0dab8a3963f814fd
Adrien Bocquet <adrienbocquet38@gmail.com>
2019-11-05 21:53:16 -0500

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from "./Navbar";
 
 const App: React.FC = () => {
     return (
         <div className="App">
+            <Navbar/>
             <p>Hi there</p>
         </div>
     );
