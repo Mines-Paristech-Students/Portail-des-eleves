7b0129e345d5d4a7d636861273b0fd156792b09c
Pierre <pierre.wan-fat@mines-paristech.fr>
2019-11-08 23:08:22 +0800

    Fix according to the remarks of @abocquet.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 
 import associations.urls
 import authentication.urls
-import chat.urls
 import polls.urls
 import repartitions.urls
 import subscriptions.urls
@@ -12,7 +11,6 @@ import tags.urls
 urlpatterns = [
     path("associations/", include(associations.urls)),
     path("", include(authentication.urls)),
-    path("chat/", include(chat.urls)),
     path("polls/", include(polls.urls)),
     path("repartitions/", include(repartitions.urls)),
     path("subscriptions/", include(subscriptions.urls)),
