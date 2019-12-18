ae71348eeb0be532f7333f514159faad3a9a309e
Sheepsody <44940387+Sheepsody@users.noreply.github.com>
2019-12-18 22:04:15 +0000

    Tests baseline with authentification

--- chat_server/simple_server.ts
+++ chat_server/simple_server.ts
@@ -1,5 +1,11 @@
+'use strict'
+
 import express from "express";
-import { createServer} from 'http';
+import { createServer } from 'http';
+const socketio_jwt = require('socketio-jwt');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
@@ -7,19 +13,34 @@ app.set("port", process.env.PORT || 3000);
 export let httpServer = createServer(app);
 export let io = require('socket.io')(httpServer);
 
-io
-.sockets.on('connection', function(socket){
-    console.log('a user connected');
+var options = {
+    secret: 'aaafoo',
+    timeout: 1000,
+    handshake: true
+};
 
-    socket.on('message', function(message) {
-        console.log(message);
-    });
+app.use(bodyParser.json());
 
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-    });
+app.post('/login', (req, res) => {
+    const profile = {
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'john@doe.com',
+        id: 123
+    };
+
+    // We are sending the profile inside the token
+    const token = jwt.sign(profile, options.secret, { expiresIn: 60 * 60 * 5 });
+    res.json({ token: token });
+});
+
+// Using handshake
+io.use(socketio_jwt.authorize(options));
+
+io.sockets.on('echo', (m) => {
+    io.sockets.emit('echo-response', m);
 });
 
 export let server = httpServer.listen(3000, () => {
-  console.log("listening on *:3000");
+    console.log("listening on *:3000");
 });
--- chat_server/test.ts
+++ chat_server/test.ts
@@ -6,36 +6,70 @@
  * Cannot read/write without the token
  */
 
+/**
+ * TODO : fix the port to have a right name for it
+ */
+
+// Just look at the official repo for the loogin
+
 var io = require('socket.io-client');
 
-import { should, assert, expect} from 'chai';
+// Load before unless triggers a timeout
+var tested = require('./simple_server')
 
-describe("Testing server", () => {
+import { should, assert, expect } from 'chai';
+const request = require('request');
+
+// Yet I'm just creating an agent
+// The rest of the work is already manager by the official repo's tests
+describe("Testing socket.io with authentification", () => {
+
   var socketURL = 'http://localhost:3000';
-  
-  var options ={
+
+  var options = {
     transports: ['websocket'],
     'force new connection': true
   };
 
   var server;
 
-  before("Setting the server", function(done) {
-    server = require('./simple_server').server;
+  before("Setting the server", function (done) {
+    server = tested.server;
     done();
   });
 
-  after("Closing the server", function(done) {
+  after("Closing the server", function (done) {
     server.close();
     done();
   });
 
-  it("Should connect", function(done) {
-    var client = io.connect(socketURL, options);
-    client.once("connect", () => {
-      client.disconnect();
-      done();
+
+  // First test
+  describe("When user logged in", function () {
+
+    // Signing the token
+    before("Logging the user", function (done) {
+      request.post({
+        url: 'http://localhost:3000/login',
+        form: { username: 'jose', password: 'Pa123' },
+        json: true
+      }, (err, resp, body) => {
+        this.token = body.token;
+        done();
+      });
+    });
+
+    it("Connecting a client", function (done) {
+      const socket = io.connect('http://localhost:3000', {
+        forceNew: true,
+        query: 'token=' + this.token
+      });
+      socket
+        .on('connect', () => {
+          socket.close();
+          done();
+        })
+        .on('error', done);
     });
   });
-  
 });
