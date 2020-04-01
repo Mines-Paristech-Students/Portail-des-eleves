f8b3d20c0873e6b44b4d4e9f73da851350f53767
Sheepsody <vvialard@hotmail.fr>
2020-04-01 18:50:03 -0400

    Added socket, hardcoded auth

--- frontend/src/chat/Message.tsx
+++ frontend/src/chat/Message.tsx
@@ -1,17 +1,17 @@
-import React, { useContext } from "react";
+import React from "react";
 
-export const Message = ({ me, sender, content }) => {
-    useContext();
-    
+export const Message = ({ me, data }) => {
     let div_class = "w-100 p-3 d-flex flex-row";
     div_class += me ? "-reverse pl-5" : " pr-5";
 
     return (
             <div className={div_class}>
                 <span className="avatar rounded-circle">
-                    {sender}
+                    {data.username}
                 </span>
-                <p className="mr-1 ml-1 border rounded-sm">{content}</p>
+                <p className="mr-1 ml-1 border rounded-sm">
+                    {data.message}
+                </p>
             </div>
     )
 };
