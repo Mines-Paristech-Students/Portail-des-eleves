2ef5d702a49dccda9eda65f65a686fccdcdd89a3
Sheepsody <vvialard@hotmail.fr>
2020-04-06 16:47:14 -0400

    Use django compatible public keys on index.js

--- chat_server/index.ts
+++ chat_server/index.ts
@@ -24,23 +24,30 @@ let port = process.env.PORT || 3001;
 export let httpServer = createServer(app);
 export let io = require('socket.io')(httpServer);
 
+// Public key from django
+const public_key = process.env.JWT_PUBLIC_KEY;
+if (public_key === undefined) {
+	throw new Error("Environnement variable JWT_PUBLIC_KEY cannot be empty \n Please check the .env file !");
+}
+
 // Authentification using handshake
 let jwtOption = {
-	"secret": process.env.JWT_SECRET,
-	// "timeout": 1000,
+	"secret": process.env.JWT_PUBLIC_KEY, // We use the public key
 	"handshake": true
 };
 
+
+// JWT auth middleware
 io.use(socketio_jwt.authorize(jwtOption));
 
+// Main Socket
 io.sockets
 	.on('connection', (socket) => {
 
 		console.log('New user connected !');
 
 		socket.on("message", async (request: any) => {
-			console.log("new message : ");
-			console.log(request.message);
+			console.log("Received new message : %s", request.message);
 			if (request.message === undefined) {
 				return
 			}
@@ -49,7 +56,7 @@ io.sockets
 				return
 			}
 
-			let model = new Message(socket.decoded_token.username, request.message, new Date());
+			let model = new Message(socket.decoded_token.user, request.message, new Date());
 			await db.add(model.username, model.message);
 
 			// The message is sended to everyone (including sender)
