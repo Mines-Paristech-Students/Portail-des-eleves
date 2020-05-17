f8ea630c4e2df99cc43bb832f2be787b4650e378
Pierre Wan-Fat <Piwakk@users.noreply.github.com>
2020-05-17 12:06:06 +0200

    [userProfile] Add `Avatar` and Timeline components. (#164)

---
+++ frontend/src/components/utils/avatar/Avatar.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Size } from "../../../utils/size";
+import { TablerColor } from "../../../utils/colors";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+
+/**
+ * Display an avatar.
+ * @param url optional, the URL to the avatar.
+ * @param size optional, the size of the avatar, a `Size`. Defaults to `Large`.
+ * @param backgroundColor optional, the color of the avatar, a `TablerColor`. Defaults to `Gray`.
+ * @param className optional, the classes to add to `Avatar`.
+ * @param tooltip optional, a tooltip to display when the avatar is hovered.
+ * @param children optional, something to display in the avatar.
+ */
+export const Avatar = ({
+    url = "",
+    size = Size.Large,
+    backgroundColor = TablerColor.Gray,
+    className,
+    tooltip,
+    children,
+}: {
+    url?: string;
+    size?: Size;
+    backgroundColor?: TablerColor;
+    className?: string;
+    tooltip?: string;
+    children?: any;
+}) => {
+    const avatar = (
+        <span
+            className={`avatar avatar-${size} avatar-${backgroundColor} ${
+                className ? className : ""
+            }`}
+            style={{
+                boxShadow: "0 2px 4px 0 hsla(0, 0%, 0%, 0.2)",
+                backgroundImage: url ? `url(${url}.jpg)` : "",
+            }}
+        >
+            {children}
+        </span>
+    );
+
+    return tooltip ? (
+        <OverlayTrigger
+            placement={"bottom"}
+            overlay={<Tooltip id={`tooltip-${url}`}>{tooltip}</Tooltip>}
+        >
+            {avatar}
+        </OverlayTrigger>
+    ) : (
+        avatar
+    );
+};
---
+++ frontend/src/components/utils/avatar/UserAvatar.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Size } from "../../../utils/size";
+import { TablerColor } from "../../../utils/colors";
+import { Avatar } from "./Avatar";
+import { Link } from "react-router-dom";
+import "./user_avatar.css";
+
+/**
+ * Thin wrapper over `Avatar` to display the avatar of an user.
+ *
+ * @param userId the user ID.
+ * @param size optional, the size of the avatar, a `Size`. Defaults to `Large`.
+ * @param backgroundColor optional, the color of the avatar, a `TablerColor`. Defaults to `Gray`.
+ * @param className optional, the classes to add to `Avatar`.
+ * @param tooltip optional, a tooltip to display when the avatar is hovered.
+ * @param link optional. If true, add a link to the user profile. Defaults to true.
+ */
+export const UserAvatar = ({
+    userId,
+    size,
+    backgroundColor,
+    className,
+    tooltip,
+    link = true,
+}: {
+    userId: string;
+    size?: Size;
+    backgroundColor?: TablerColor;
+    className?: string;
+    tooltip?: string;
+    link?: boolean;
+}) => {
+    const avatar = (
+        <Avatar
+            url={`profile/${userId}`}
+            size={size}
+            backgroundColor={backgroundColor}
+            className={className}
+            tooltip={tooltip}
+        />
+    );
+    return link ? (
+        <Link to={`/profils/${userId}`} className="user-avatar-link">
+            {avatar}
+        </Link>
+    ) : (
+        avatar
+    );
+};
