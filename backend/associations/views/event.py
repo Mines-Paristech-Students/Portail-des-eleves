729a6d8e7f1f97ba3ebe0c0d598ff4d77d8953fa
Pierre <pierre.wan-fat@mines-paristech.fr>
2020-05-23 00:59:57 +0200

    [Events_frontend] Add backend filters.

--- backend/associations/views/event.py
+++ backend/associations/views/event.py
@@ -1,3 +1,11 @@
+from datetime import datetime
+
+from django.db.models import Q
+from django_filters.rest_framework import (
+    DateTimeFromToRangeFilter,
+    FilterSet,
+    CharFilter,
+)
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -7,10 +15,38 @@ from associations.permissions import EventsPermission, JoinEventPermission
 from associations.serializers import EventSerializer, ReadOnlyEventSerializer
 
 
+class EventFilter(FilterSet):
+    starts_at = DateTimeFromToRangeFilter()
+    ends_at = DateTimeFromToRangeFilter()
+    current = CharFilter(method="filter_current")
+
+    class Meta:
+        model = Event
+        fields = ("starts_at", "ends_at", "current", "association")
+
+    def filter_current(self, queryset, _, value):
+        condition = Q(starts_at__lte=datetime.now()) & Q(ends_at__gte=datetime.now())
+
+        if value.lower() == "true" or len(value) == 0:
+            return queryset.filter(condition)
+        else:
+            return queryset.exclude(condition)
+
+
 class EventViewSet(viewsets.ModelViewSet):
+    """
+        Filters:
+            - starts_at_before / starts_at_end: use a datetime like `2016-01-01 8:00` or a date like `2016-01-01`.
+            - ends_at_before / ends_at_end: use a datetime like `2016-01-01 8:00` or a date like `2016-01-01`.
+            - current: if set to `true`, only keep the currently ongoing events.
+            - association: filter the organizing association.
+    """
+
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = (EventsPermission,)
+    filterset_class = EventFilter
+    ordering_fields = ["starts_at"]
 
     def get_serializer_class(self):
         if self.action in ("create", "update", "partial_update"):
