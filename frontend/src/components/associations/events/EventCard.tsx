8c04c0f470380b6b3dd3c74eade6d697a565f83d
Pierre <pierre.wan-fat@mines-paristech.fr>
2020-05-23 12:45:33 +0200

    [Events_frontend] Cosmetic changes to EventCard.

--- frontend/src/components/associations/events/EventCard.tsx
+++ frontend/src/components/associations/events/EventCard.tsx
@@ -3,13 +3,10 @@ import React, { useContext, useState } from "react";
 import { Event } from "../../../models/associations/event";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import OverlayTrigger from "react-bootstrap/OverlayTrigger";
-import Tooltip from "react-bootstrap/Tooltip";
 import { Link } from "react-router-dom";
 import { Association } from "../../../models/associations/association";
 import { AvatarList } from "../../utils/avatar/AvatarList";
 import { UserAvatar } from "../../utils/avatar/UserAvatar";
-import { formatDate, formatTime } from "../../../utils/format";
 import { Size } from "../../../utils/size";
 import { Avatar } from "../../utils/avatar/Avatar";
 import { EventCardModal } from "./EventCardModal";
@@ -17,50 +14,16 @@ import { queryCache, useMutation } from "react-query";
 import { api } from "../../../services/apiService";
 import { ToastContext, ToastLevel } from "../../utils/Toast";
 import { AxiosError } from "axios";
+import { CardStatus } from "../../utils/CardStatus";
+import { TablerColor } from "../../../utils/colors";
+import { EventDate } from "./EventDate";
 
-const EventDate = ({ event }: { event: Event }) => {
-    if (
-        event.startsAt.getFullYear() === event.endsAt.getFullYear() &&
-        event.startsAt.getMonth() === event.endsAt.getMonth() &&
-        event.startsAt.getDate() === event.endsAt.getDate()
-    ) {
-        return (
-            <>
-                {formatDate(event.startsAt)}, {formatTime(event.startsAt)} —{" "}
-                {formatTime(event.endsAt)}
-            </>
-        );
-    } else {
-        return (
-            <>
-                <OverlayTrigger
-                    placement={"bottom"}
-                    overlay={
-                        <Tooltip id={`tooltip-${event.id}-start`}>
-                            {formatDate(event.startsAt)},{" "}
-                            {formatTime(event.startsAt)}
-                        </Tooltip>
-                    }
-                >
-                    <span>{formatDate(event.startsAt)}</span>
-                </OverlayTrigger>{" "}
-                —{" "}
-                <OverlayTrigger
-                    placement={"bottom"}
-                    overlay={
-                        <Tooltip id={`tooltip-${event.id}-end`}>
-                            {formatDate(event.endsAt)},{" "}
-                            {formatTime(event.endsAt)}
-                        </Tooltip>
-                    }
-                >
-                    <span>{formatDate(event.endsAt)}</span>
-                </OverlayTrigger>
-            </>
-        );
-    }
-};
-
+/**
+ * Display an Event in a Card, as well as a link to join / leave the event.
+ * The date, place, name and description of the event are displayed, as well as the participants. One can click on the
+ * participants to open `EventCardModel` (which should display more information about the participants).
+ * An edit button may be added.
+ */
 export const EventCard = ({
     association,
     event,
@@ -72,8 +35,11 @@ export const EventCard = ({
     userId?: string;
     canEdit?: boolean;
 }) => {
+    const isOver = () => event.endsAt > new Date();
+
     const newToast = useContext(ToastContext);
     const [showModal, setShowModal] = useState(false);
+
     const [join] = useMutation(api.events.join, {
         onSuccess: (response) => {
             queryCache.refetchQueries(["events.list"]);
@@ -133,29 +99,38 @@ export const EventCard = ({
                     handleClose={() => setShowModal(false)}
                 />
             )}
+
+            <CardStatus
+                color={isOver() ? TablerColor.Blue : TablerColor.Gray}
+            />
+
             <Card.Header>
                 <Card.Title>{event.name}</Card.Title>
 
                 <div className="card-options">
-                    {userId &&
-                    event.participants
-                        .map((participant) => participant.id)
-                        .includes(userId) ? (
-                        <Button
-                            className="btn-sm"
-                            variant="secondary"
-                            onClick={() => leave({ eventId: event.id })}
-                        >
-                            Se désinscrire
-                        </Button>
-                    ) : (
-                        <Button
-                            className="btn-sm"
-                            variant="primary"
-                            onClick={() => join({ eventId: event.id })}
-                        >
-                            S’inscrire
-                        </Button>
+                    {isOver() && (
+                        <>
+                            {userId &&
+                            event.participants
+                                .map((participant) => participant.id)
+                                .includes(userId) ? (
+                                <Button
+                                    className="btn-sm"
+                                    variant="secondary"
+                                    onClick={() => leave({ eventId: event.id })}
+                                >
+                                    Se désinscrire
+                                </Button>
+                            ) : (
+                                <Button
+                                    className="btn-sm"
+                                    variant="primary"
+                                    onClick={() => join({ eventId: event.id })}
+                                >
+                                    S’inscrire
+                                </Button>
+                            )}
+                        </>
                     )}
 
                     {canEdit && (
@@ -195,7 +170,9 @@ export const EventCard = ({
                         )}
                     </AvatarList>
                 ) : (
-                    <p className="text-muted mb-4">Pas encore d’inscription.</p>
+                    <p className="text-muted mb-4">
+                        Pas {isOver() ? "encore " : ""}d’inscription.
+                    </p>
                 )}
 
                 <p>{event.description}</p>
---
+++ frontend/src/components/associations/events/EventDate.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Event } from "../../../models/associations/event";
+import { formatDate, formatTime } from "../../../utils/format";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+
+/**
+ * Display a formatted event date, depending on whether or not startsAt and endsAt are the same day.
+ */
+export const EventDate = ({ event }: { event: Event }) => {
+    if (
+        event.startsAt.getFullYear() === event.endsAt.getFullYear() &&
+        event.startsAt.getMonth() === event.endsAt.getMonth() &&
+        event.startsAt.getDate() === event.endsAt.getDate()
+    ) {
+        return (
+            <>
+                {formatDate(event.startsAt)}, {formatTime(event.startsAt)} —{" "}
+                {formatTime(event.endsAt)}
+            </>
+        );
+    } else {
+        return (
+            <>
+                <OverlayTrigger
+                    placement={"bottom"}
+                    overlay={
+                        <Tooltip id={`tooltip-${event.id}-start`}>
+                            {formatDate(event.startsAt)},{" "}
+                            {formatTime(event.startsAt)}
+                        </Tooltip>
+                    }
+                >
+                    <span>{formatDate(event.startsAt)}</span>
+                </OverlayTrigger>{" "}
+                —{" "}
+                <OverlayTrigger
+                    placement={"bottom"}
+                    overlay={
+                        <Tooltip id={`tooltip-${event.id}-end`}>
+                            {formatDate(event.endsAt)},{" "}
+                            {formatTime(event.endsAt)}
+                        </Tooltip>
+                    }
+                >
+                    <span>{formatDate(event.endsAt)}</span>
+                </OverlayTrigger>
+            </>
+        );
+    }
+};
