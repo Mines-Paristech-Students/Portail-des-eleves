bc0c6b7b7d82ea90020eba17c1aae7fef63306e9
Adrien Bocquet <adrienbocquet38@gmail.com>
2020-05-24 13:14:01 +0200

    Fix #400

--- frontend/src/components/associations/marketplace/counter/RefundForm.tsx
+++ frontend/src/components/associations/marketplace/counter/RefundForm.tsx
@@ -1,8 +1,16 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { ToastContext, ToastLevel } from "../../../utils/Toast";
 import { queryCache, useMutation } from "react-query";
 import { api } from "../../../../services/apiService";
-import { Button, Col, Form, Row, InputGroup } from "react-bootstrap";
+import * as Yup from "yup";
+import {
+    Button,
+    Col,
+    Form as FormBoostrap,
+    Row,
+} from "react-bootstrap";
+import { Form, Formik } from "formik";
+import { TextFormGroup } from "../../../utils/forms/TextFormGroup";
 
 export const RefundForm = ({ customer, marketplaceId }) => {
     const newToast = useContext(ToastContext);
@@ -14,7 +22,6 @@ export const RefundForm = ({ customer, marketplaceId }) => {
                 level: ToastLevel.Success,
             });
             queryCache.refetchQueries("marketplace.balance");
-            setRefundValue("");
         },
         onError: (err) => {
             newToast({
@@ -24,48 +31,47 @@ export const RefundForm = ({ customer, marketplaceId }) => {
         },
     });
 
-    const increaseBalance = () => {
-        let value = parseFloat(refundValue);
-        if (value <= 0) {
-            newToast({
-                message: `Vous devez créditer un compte d'un nombre positif`,
-                level: ToastLevel.Error,
-            });
-            return;
-        }
-
+    const increaseBalance = ({ refund }) => {
         createFunding({
             marketplaceId,
             customerId: customer.id,
-            value: value,
+            value: refund,
         });
     };
 
-    const [refundValue, setRefundValue] = useState("");
-
     return (
-        <Form.Group>
-            <Row>
-                <Col>
-                    <InputGroup>
-                        <Form.Control
-                            type="number"
-                            placeholder="recréditer le compte"
-                            value={refundValue}
-                            onChange={(e) => setRefundValue(e.target.value)}
-                        />
-                        <InputGroup.Append>
-                            <InputGroup.Text>€</InputGroup.Text>
-                        </InputGroup.Append>
-                    </InputGroup>
-                </Col>
-                <Col xs="auto">
-                    <Button variant={"success"} onClick={increaseBalance}>
-                        <span className="fe fe-plus " />
-                        Créditer
-                    </Button>
-                </Col>
-            </Row>
-        </Form.Group>
+        <Formik
+            initialValues={{ refund: "" }}
+            validationSchema={Yup.object().shape({
+                refund: Yup.number()
+                    .required("Veuillez entrer un nombre")
+                    .moreThan(
+                        0,
+                        "Un solde doit être recrédité d'un nombre positif"
+                    ),
+            })}
+            onSubmit={increaseBalance}
+        >
+            <Form>
+                <FormBoostrap.Group>
+                    <Row>
+                        <Col>
+                            <TextFormGroup
+                                name="refund"
+                                type="number"
+                                placeholder="recréditer le compte"
+                                textRight={"€"}
+                            />
+                        </Col>
+                        <Col xs="auto">
+                            <Button variant={"success"} type={"submit"} className={"m-2"}>
+                                <span className="fe fe-plus " />
+                                Créditer
+                            </Button>
+                        </Col>
+                    </Row>
+                </FormBoostrap.Group>
+            </Form>
+        </Formik>
     );
 };
