279b8dc2e66a6c4ebcd47f568d9957d225d15160
Adrien Bocquet <adrienbocquet38@gmail.com>
2020-05-24 20:20:20 +0200

    Remove legacy frontend

--- frontend_legacy/src/app/rer-timetable/next-time.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { WaitTimePipe } from './next-time.pipe';
-
-describe('WaitTimePipe', () => {
-  it('create an instance', () => {
-    const pipe = new WaitTimePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
