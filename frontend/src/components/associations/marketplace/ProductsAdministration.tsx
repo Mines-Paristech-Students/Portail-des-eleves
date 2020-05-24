41030242c13189aec03aeacfda57ff490da86198
Adrien Bocquet <adrienbocquet38@gmail.com>
2020-05-24 17:55:35 +0200

    Display products into an array

--- frontend/src/components/associations/marketplace/ProductsAdministration.tsx
+++ frontend/src/components/associations/marketplace/ProductsAdministration.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { PageTitle } from "../../utils/PageTitle";
 import { Pagination } from "../../utils/Pagination";
 import { api } from "../../../services/apiService";
-import Row from "react-bootstrap/Row";
 import { Product } from "./Product";
 import { Link } from "react-router-dom";
+import { decidePlural } from "../../../utils/format";
+import { Table } from "../../utils/table/Table";
+import { Card } from "react-bootstrap";
 
 export const AssociationMarketplaceProductAdministration = ({
     association,
@@ -13,32 +15,70 @@ export const AssociationMarketplaceProductAdministration = ({
     return (
         <>
             <PageTitle>Produits</PageTitle>
-            <Row>
-                <Pagination
-                    apiKey={["products.list", marketplaceId, { page_size: 8 }]}
-                    apiMethod={api.products.list}
-                    render={(products, controlbar) => (
-                        <>
-                            <Row>
-                                {products.map((product) => (
-                                    <Product
-                                        product={product}
-                                        key={product.id}
-                                        additionalContent={
-                                            <Link
-                                                to={`/associations/${marketplaceId}/magasin/produits/${product.id}/modifier`}
-                                            >
-                                                Modifier
-                                            </Link>
-                                        }
-                                    />
-                                ))}
-                            </Row>
-                            {controlbar}
-                        </>
-                    )}
-                />
-            </Row>
+
+            <Pagination
+                apiKey={["products.list", marketplaceId, { page_size: 8 }]}
+                apiMethod={api.products.list}
+                paginationControlProps={{
+                    className: "justify-content-center mt-5",
+                }}
+                render={(products, paginationControls) => (
+                    <>
+                        <Card>
+                            <Table
+                                columns={columns(marketplaceId)}
+                                data={products}
+                            />
+                        </Card>
+                        {paginationControls}
+                    </>
+                )}
+            />
         </>
     );
 };
+
+const columns = (marketplaceId) => [
+    {
+        key: "product",
+        header: "Product",
+        render: (product) => product.name,
+    },
+    {
+        key: "price",
+        header: "Prix",
+        cellClassName: "text-muted",
+        render: (product) => product.price + "€",
+    },
+    {
+        key: "description",
+        header: "Description",
+        cellClassName: "flex-shrink-1",
+    },
+    {
+        key: "stocks",
+        header: "Stocks",
+        cellClassName: "text-muted",
+        render: (product) =>
+            product.numberLeft > -1
+                ? ` ${product.numberLeft} ${decidePlural(
+                      product.numberLeft,
+                      "restant",
+                      "restants"
+                  )}`
+                : "Stocks infinis",
+    },
+    {
+        key: "action",
+        header: "Actions",
+        render: (product) => (
+            <Link
+                to={`/associations/${marketplaceId}/magasin/produits/${product.id}/modifier`}
+            >
+                Modifier
+            </Link>
+        ),
+        headerClassName: "text-right",
+        cellClassName: "text-right",
+    },
+];
