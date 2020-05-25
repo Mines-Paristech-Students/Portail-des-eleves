1c423c68b5f4e184566b8f1d078b30c40244180e
Sheepsody <vvialard@hotmail.fr>
2020-05-25 16:34:18 +0200

    Can list questions#

--- frontend/src/pages/courses/forms/Edit.tsx
+++ frontend/src/pages/courses/forms/Edit.tsx
@@ -1,31 +1,44 @@
 import React, { useContext } from "react";
-import { Form, Row, Col, Button, Modal } from "react-bootstrap";
+import { Form, Row, Col, Button, Modal, Container } from "react-bootstrap";
 import { Form as FormModel } from "../../../models/courses/form"
+import { PageTitle } from "../../../utils/common";
 import { api, useBetterQuery } from "../../../services/apiService";
 import { useFormik } from "formik";
 import { ToastContext, ToastLevel } from "../../../utils/Toast";
 import { useParams } from "react-router-dom";
+import { Question } from "../../../models/courses/question";
 
 
 export const EditCourseForm = ({ course }) => {
-    const { data: form, error, status } = useBetterQuery<boolean>(
-        "courses.has_voted",
-        api.courses.has_voted,
+    const { data: questions, error, status } = useBetterQuery<Question[]>(
+        "courses.questions",
+        api.courses.questions,
         course.id,
     );
-    {/*  
-    TODOs
-    1. Fetch questions
-    */}
 
-    return (
-        <p></p>
-    )
+    if (status === "loading") return <p>Chargement des cours</p>;
+    if (status === "error") return `Something went wrong: ${error}`;
+    if (status === "success" && questions) {
+        return (
+            <Container>
+                <PageTitle>Cours</PageTitle>
+
+                {/* Fetch from another form */}
+
+                <Row>
+                    {questions.map(question =>
+                        <QuestionEditor question={question} />
+                    )}
+                </Row>
+            </Container>
+        );
+    }
+
+    return <p>Not a single question yet</p>;
 }
 
-
-export const PrefillItem = () => {
+export const QuestionEditor = ({ question }) => {
     return (
-        <p>To be done</p>
+        <p>{question.label}</p>
     )
 }
