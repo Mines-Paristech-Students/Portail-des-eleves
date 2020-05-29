f0261c30696d629d4e711d3860da9706bc99186e
Sheepsody <vvialard@hotmail.fr>
2020-05-29 15:24:41 +0200

    Added tests questions

--- backend/courses/tests/test_comments.py
+++ backend/courses/tests/test_comments.py
@@ -7,12 +7,20 @@ from backend.tests_utils import WeakAuthenticationBaseTestCase
 class CommentTestCase(WeakAuthenticationBaseTestCase):
     fixtures = ["test_authentication.yaml", "test_comments.yaml"]
 
-    EXPECTED_FIELDS = {
+    EXPECTED_FIELDS = [
         "count",
         "next",
         "previous",
         "results",
-    }
+    ]
+
+    EXPECTED_FIELDS_COMMENT =[
+        "id",
+        "question",
+        "course",
+        "content",
+        "date",
+    ]
 
     ALL_USERS = ["17admin", "17simple"]
 
@@ -26,11 +34,56 @@ class CommentTestCase(WeakAuthenticationBaseTestCase):
     # LIST #
     ########
 
-    def test_if_not_logged_in_then_cannot_list(self):
+    def test_if_not_logged_in_then_cannot_list_comments(self):
         res = self.list()
         self.assertStatusCode(res, 404)
 
-    def if_logged_in_then_can_list(self):
+    ###########
+    # OPTIONS #
+    ###########
+
+    # All these should return 1 results as in test_comments.yaml
+
+    filter_date = '2100-04-20T22:10:57.577Z'
+    filter_question = 1
+    filter_course = 2
+
+    def if_logged_in_then_can_list_all_comments(self):
         self.login("17simple")
         res = self.list()
         self.assertStatusCode(res, 200)
+
+        # Pagination default is 5
+        self.assertEqual(len(res.data), 5)
+
+        for key in self.EXPECTED_FIELDS:
+            self.assertIsNotNone(res.data[key])
+        
+        # Because of the ordering, we should get the most recent first
+        last_comment = res.data["results"][0]        
+
+        for key in self.EXPECTED_FIELDS_COMMENT:
+            self.assertIsNotNone(last_comment[key])
+
+        self.assertEqual(last_comment["content"], "e")
+
+    def if_logged_in_then_can_list_comments_by_question(self):
+        self.login("17simple")
+        res = self.list(data={"question": self.filter_question})
+
+        self.assertStatusCode(res, 200)
+        self.assertEqual(len(res.data), 1)
+
+    def if_logged_in_then_can_list_comments_by_date(self):
+        self.login("17simple")
+        res = self.list(data={"question": self.filter_date})
+
+        self.assertStatusCode(res, 200)
+        self.assertEqual(len(res.data), 1)
+
+    def if_logged_in_then_can_list_comments_by_course(self):
+        self.login("17simple")
+        res = self.list(data={"question": self.filter_course})
+
+        self.assertStatusCode(res, 200)
+        self.assertEqual(len(res.data), 1)
