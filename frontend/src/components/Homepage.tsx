021bb70d61f316899f8c8fa6284673e2c601a0c4
Pierre <pierre.wan-fat@mines-paristech.fr>
2020-05-30 12:55:13 +0200

    WIP

--- frontend/src/components/Homepage.tsx
+++ frontend/src/components/Homepage.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { PageTitle } from "./utils/PageTitle";
 import Container from "react-bootstrap/Container";
 import { Chat } from "../chat/Chat";
+import { Test } from "./Test";
 
 export const Homepage = () => {
     return (
         <Container>
             <PageTitle>Homepage</PageTitle>
+            <Test />
             <Chat />
         </Container>
     );
