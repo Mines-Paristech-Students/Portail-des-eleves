e1f9cd232452fb570d7eb6357950640c2b526d81
Adrien Bocquet <adrienbocquet38@gmail.com>
2020-05-31 14:35:28 +0200

    remove API parameters and API key distinction

--- frontend/src/components/associations/medias/List.tsx
+++ frontend/src/components/associations/medias/List.tsx
@@ -36,8 +36,11 @@ export const AssociationFilesystemList = ({ association }) => {
             }
         >
             <Pagination
-                apiKey={["medias.list", associationId]}
-                apiParameters={tagParams}
+                apiKey={[
+                    "medias.list",
+                    associationId,
+                    { ...tagParams, page_size: 2 },
+                ]}
                 apiMethod={api.medias.list}
                 render={(medias, paginationControl) => (
                     <>
