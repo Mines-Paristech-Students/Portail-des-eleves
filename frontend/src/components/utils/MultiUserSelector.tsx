1b1532edda207a9af4c8a6b5c23785e8c7a45eea
Adrien Bocquet <adrienbocquet38@gmail.com>
2020-06-02 07:22:46 +0200

    WIP

---
+++ frontend/src/components/utils/MultiUserSelector.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+export const MultiUserSelector = ({ onChange }) => {
+    const [searchValue, setSearchValue] = useState("");
+    const [selectedUsers, setSelectedUsers]
+
+};
