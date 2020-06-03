569fb30a27c2e96e1e2f5a4e79f47ccc2b733f41
Pierre Wan-Fat <Piwakk@users.noreply.github.com>
2020-06-03 21:33:50 +0200

    Refactor date picker (#224)
    * [refactorFormUtils] Refactor `TextFormGroup`.
    * [refactorDatePicker] Refactor `DatePickerField` and add
    `DatePickerFormGroup`.
    * [refactorDatePicker] Remove useless CSS.
    * [refactorDatePicker] Rename to `DayPickerField`.
    * [refactorDayPickerInput] Refactor DayPickerInputField.
    * Refactor `DayTimePicker`
    * Add disabled prop.

--- frontend/src/components/utils/forms/BaseFormGroup.tsx
+++ frontend/src/components/utils/forms/BaseFormGroup.tsx
@@ -5,6 +5,7 @@ import { useFormikContext } from "formik";
 export type BaseFormGroupProps = {
     name: string;
     label?: string;
+    help?: string;
     feedback?: boolean;
     feedbackOnTouchedOnly?: boolean;
     formGroupProps?: any;
@@ -14,12 +15,14 @@ export type BaseFormGroupProps = {
 
 /**
  * A base component wrapping its `children` in a `Form.Group` component.
- * Also includes optional `Form.Label` and `Form.Control.Feedback` components.
+ * Also includes optional `Form.Label`, `Form.Control.Feedback` and help text
+ * components.
  * The `children` is usually a component using the Formik `Field` logic.
  *
  * @param name the name of the control, used for getting the error in the Formik
  * context.
  * @param label optional, the label of the form control.
+ * @param help optional, a text to display below the form control.
  * @param feedback defaults to `true`. If `true`, a `Form.Control.Feedback` is
  * displayed and filled with the errors obtained from the Formik context.
  * @param feedbackOnTouchedOnly defaults to `true`. If `true`, the feedback
@@ -33,6 +36,7 @@ export type BaseFormGroupProps = {
 export const BaseFormGroup = ({
     name,
     label,
+    help,
     feedback = true,
     feedbackOnTouchedOnly = true,
     formGroupProps,
@@ -47,6 +51,8 @@ export const BaseFormGroup = ({
             {label && <Form.Label {...labelProps}>{label}</Form.Label>}
 
             {children}
+
+            {help && <p className="form-text text-muted small mb-0">{help}</p>}
 
             {feedback &&
                 (touched[name] || !feedbackOnTouchedOnly) &&
---
+++ frontend/src/components/utils/forms/DayPickerField.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { FieldAttributes, useField } from "formik";
+import DayPicker from "react-day-picker";
+import "react-day-picker/lib/style.css";
+import { MONTHS, WEEKDAYS_LONG, WEEKDAYS_SHORT } from "../../../utils/format";
+
+export const dayPickerLocalisationProps = (todayButton = false) => ({
+    todayButton: todayButton ? "Aujourd’hui" : "",
+    locale: "fr",
+    months: MONTHS,
+    weekdaysLong: WEEKDAYS_LONG,
+    weekdaysShort: WEEKDAYS_SHORT,
+    firstDayOfWeek: 1,
+});
+
+export type DatePickerFieldProps = {
+    name: string;
+    todayButton?: boolean;
+} & FieldAttributes<any>;
+
+/**
+ * A `DayPicker` component tied to a Formik field.
+ *
+ * @param name the name of the control, given to Formik's `useField`.
+ * @param todayButton defaults to `false`. If `true`, a “Today” button is displayed.
+ * @param props passed to `useField` and `Form.Control`.
+ */
+export const DayPickerField = ({
+    name,
+    todayButton = false,
+    ...props
+}: DatePickerFieldProps) => {
+    const [field, , helper] = useField<Date | undefined>({
+        name: name,
+        ...props,
+    });
+
+    return (
+        <DayPicker
+            {...field}
+            month={field.value}
+            selectedDays={field.value}
+            onDayClick={(day) => helper.setValue(day)}
+            {...dayPickerLocalisationProps(todayButton)}
+        />
+    );
+};
