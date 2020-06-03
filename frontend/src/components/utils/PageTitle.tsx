b64f1d85a202f4a40f189f5ef4660ba70b6c43b4
Pierre <pierre.wan-fat@mines-paristech.fr>
2020-06-04 00:21:24 +0200

    Prettier

--- frontend/src/components/utils/PageTitle.tsx
+++ frontend/src/components/utils/PageTitle.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-export const PageTitle = ({className, children}: {className?: string; children: any}) => (
+export const PageTitle = ({
+    className,
+    children,
+}: {
+    className?: string;
+    children: any;
+}) => (
     <div className={className || "mt-2 mb-2"}>
         <h1 className="page-title">{children}</h1>
     </div>
