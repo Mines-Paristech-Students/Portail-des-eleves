043abb0c335bb73ea19b54a4bb495b85e5315d50
Adrien Bocquet <4300678+abocquet@users.noreply.github.com>
2020-06-04 18:22:49 +0200

    Update frontend/src/components/utils/MultiUserSelector.tsx
    Co-authored-by: Pierre Wan-Fat <Piwakk@users.noreply.github.com>

--- frontend/src/components/utils/MultiUserSelector.tsx
+++ frontend/src/components/utils/MultiUserSelector.tsx
@@ -36,11 +36,11 @@ export const MultiUserSelector = ({ onChange }) => {
     >();
     const [userError, setUserError] = useState<any>();
 
-    /* As we are doing many sets-like features here (check if a user is selected,
-     * change the user from list, etc.. and we make it on a non-small sample (>100)
-     * To allow faster list switching, we add a "selected" field to say in which
-     * set a user belongs and simply switch it when we want to transfer the user.
-     * The displaying of the users is made simple by using the selectedUsers and
+    /* We are doing many set operations (check if a user is selected,
+     * change the user from list... on a non-small sample (>100).
+     * To allow faster list switching, we add a "selected" field to specify which
+     * set a user belongs to and simply switch it when we want to transfer the user.
+     * Displaying the users is made simple by using the selectedUsers and
      * unselectedUsers variables.
      */
     const [users, setUsers] = useState<{
