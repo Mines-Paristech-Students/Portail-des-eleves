0fd3dc17c19c7148228a5de1f345f4272b127d4a
Victor Vialard <44940387+Sheepsody@users.noreply.github.com>
2020-06-08 18:45:11 +0200

    Fix chat backend (#219)
    * Fixing
    
    * Fixing
    
    * Updated package-lock in gitignore
    
    * Investigating time
    
    * Fixed issue timesta;p
    
    * Added intergace
    
    * Cleaned tests
    
    * Reformatted
    
    * Add recommendation
    
    * Add recommendation
    
    * Prettier
    
    * Removed migrations
    
    * Removed mongodb dependencies

--- chat_server/db.ts
+++ chat_server/db.ts
@@ -1,18 +1,23 @@
 import { Pool } from "pg";
 
+import { Message } from "./message";
+
 // All infos for pools are passed while starting the container
 const pool = new Pool();
 
 // Queries
-const create_schema_query = `CREATE TABLE IF NOT EXISTS messages (
+const create_schema_query = `
+  CREATE TABLE IF NOT EXISTS messages (
     username VARCHAR(50) NOT NULL,
-    posted_on TIMESTAMP NOT NULL,
+    posted_on TIMESTAMP,
     message TEXT NOT NULL
-  ) ;
-  CREATE UNIQUE INDEX IF NOT EXISTS index ON messages(posted_on DESC)`;
+  );
+
+  CREATE UNIQUE INDEX IF NOT EXISTS index ON messages(posted_on DESC);
+`;
 
 const add_query =
-  "INSERT INTO messages (username, message, posted_on) VALUES ($1, $2, NOW())";
+  "INSERT INTO messages (username, message, posted_on) VALUES ($1, $2, $3)";
 
 const get_query =
   "SELECT * FROM messages WHERE posted_on <= $1 ORDER BY posted_on DESC LIMIT $2";
@@ -30,12 +35,16 @@ pool
     console.info("Database schema available ✅");
   })
   .catch((err) => {
-    console.error("❌ Failed to create schema ", err);
+    console.error("Failed to create schema ❌", err);
   });
 
 // Query functions
-const add = async function add(username: string, message: string) {
-  await pool.query(add_query, [username, message]);
+const add = async function add(message: Message) {
+  await pool.query(add_query, [
+    message.username,
+    message.message,
+    message.posted_on,
+  ]);
 };
 
 let get = async function get(from: Date, limit: number) {
