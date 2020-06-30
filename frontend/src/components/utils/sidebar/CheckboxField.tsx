ed1669faa6ee253d9adc79b83dd5ceabafa2dc6b
Pierre <pierre.wan-fat@mines-paristech.fr>
2020-07-01 00:50:47 +0200

    Add filters for loans management.

--- frontend/src/components/utils/sidebar/CheckboxField.tsx
+++ frontend/src/components/utils/sidebar/CheckboxField.tsx
@@ -1,6 +1,37 @@
 import React from "react";
 import { FormCheck } from "react-bootstrap";
 
+/**
+ * Utility function useful for updating array states where each item is supposed
+ * to be managed by one checkbox.
+ *
+ * If the checkbox is checked, return `oldStatus` updated to include `value`.
+ * If it is uncheckd, return `oldStatus` updated to exclude `value`.
+ *
+ * For instance, suppose the status is an array which may contain at most the
+ * items: `["BORROWED", "AVAILABLE", "REQUESTED"]`.
+ *
+ * One may use this snippet to update `BORROWED`:
+ *
+ * ```
+ * <CheckboxField
+ * label={"Prêtés"}
+ *   state={status.includes("BORROWED")}
+ *   onChange={(checked) =>
+ *     setStatus((oldStatus) => updateStatus(checked, "BORROWED", oldStatus))
+ *   }
+ * />
+ * ```
+ *
+ * @param checked `true` if the checkbox is checked. Given by `ChecboxField`'s
+ * `onChange`.
+ * @param value the item which needs to be updated in the status array.
+ * @param oldStatus the array status to update.
+ */
+export function updateStatus<T>(checked: boolean, value: T, oldStatus: T[]) {
+  return checked ? [...oldStatus, value] : oldStatus.filter((x) => x !== value);
+}
+
 export const CheckboxField = ({ label, state, onChange }) => (
   <label className="custom-control custom-checkbox">
     <FormCheck.Input
