85e2b1fbd8f2f162d093112aa429a08052043e0b
Louis Delmas <louis.delmas@outlook.com>
2022-04-02 17:11:45 +0200

    Calendar test issue fix

--- backend/associations/tests/event/test_calendar.py
+++ backend/associations/tests/event/test_calendar.py
@@ -11,50 +11,52 @@ turn_little = widget_calendar.turn_little
 class CalendarTestCase(BaseEventsTestCase):
     events_displayed = []
 
-    # Association for the events test #
-    association = Association.objects.get(id="biero")
-
-    # Test events #
-
-    # Don't display the events which start two weeks after now ?
-    #
-    # event_starts_two_weeks_after = {
-    #     "association": association,
-    #     "name": "Grosse biéro dans 2 semaines",
-    #     "description": "On y croit",
-    #     "starts_at": timezone.now() + timedelta(weeks=2),
-    #     "ends_at": timezone.now() + timedelta(days=15),
-    #     "place": "Octo",
-    # }
-    #
-    # event_starts_two_weeks_after_little = turn_little(event_starts_two_weeks_after)
-
-    event_starts_one_week_after = {
-        "association": association,
-        "name": "Grosse biéro dans 1 semaine",
-        "description": "On y croit (pas)",
-        "starts_at": timezone.now() + timedelta(weeks=1),
-        "ends_at": timezone.now() + timedelta(days=8),
-        "place": "Octo",
-    }
-
-    event_starts_one_week_after_little = turn_little(event_starts_one_week_after)
-
-    event_ends_one_week_before = {
-        "association": association,
-        "name": "Grosse biéro...",
-        "description": "On y croit (plus)",
-        "starts_at": timezone.now() - timedelta(days=8),
-        "ends_at": timezone.now() - timedelta(weeks=1),
-        "place": "Octo",
-    }
-
-    event_ends_one_week_before_little = turn_little(event_ends_one_week_before)
-
     def setUp(self):
-        # Event.objects.create(**self.event_starts_two_weeks_after)
-        Event.objects.create(**self.event_starts_one_week_after)
-        Event.objects.create(**self.event_ends_one_week_before)
+        # Association for the events test #
+        association = Association.objects.get(id="biero")
+
+        # Test events #
+
+        # Don't display the events which start two weeks after now ?
+        #
+        # event_starts_two_weeks_after = {
+        #     "association": association,
+        #     "name": "Grosse biéro dans 2 semaines",
+        #     "description": "On y croit",
+        #     "starts_at": timezone.now() + timedelta(weeks=2),
+        #     "ends_at": timezone.now() + timedelta(days=15),
+        #     "place": "Octo",
+        # }
+        #
+        # event_starts_two_weeks_after_little = turn_little(event_starts_two_weeks_after)
+
+        event_starts_one_week_after = {
+            "association": association,
+            "name": "Grosse biéro dans 1 semaine",
+            "description": "On y croit (pas)",
+            "starts_at": timezone.now() + timedelta(weeks=1),
+            "ends_at": timezone.now() + timedelta(days=8),
+            "place": "Octo",
+        }
+
+        self.event_starts_one_week_after_little = turn_little(
+            event_starts_one_week_after
+        )
+
+        event_ends_one_week_before = {
+            "association": association,
+            "name": "Grosse biéro...",
+            "description": "On y croit (plus)",
+            "starts_at": timezone.now() - timedelta(days=8),
+            "ends_at": timezone.now() - timedelta(weeks=1),
+            "place": "Octo",
+        }
+
+        self.event_ends_one_week_before_little = turn_little(event_ends_one_week_before)
+
+        # Event.objects.create(**event_starts_two_weeks_after)
+        Event.objects.create(**event_starts_one_week_after)
+        Event.objects.create(**event_ends_one_week_before)
 
         # Get events displayed on the calendar #
         self.events_displayed = widget_calendar.events_in_calendar()
