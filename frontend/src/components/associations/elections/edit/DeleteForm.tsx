f8f68407e1cf90b2c2cf949196ff16c70e9294f1
Alexiane <alexiane.laude@mines-paristech.fr>
2022-04-08 09:31:25 +0200

    fix error message when deleting election

--- frontend/src/components/associations/elections/edit/DeleteForm.tsx
+++ frontend/src/components/associations/elections/edit/DeleteForm.tsx
@@ -18,9 +18,7 @@ export const DeleteForm = ({ election }) => {
       const error = errorAsUnknown as AxiosError;
       sendErrorToast(
         `Erreur. Merci de réessayer ou de contacter les administrateurs si cela persiste. ${
-          error.response === undefined
-            ? ""
-            : "Détails :" + error.response.data.detail
+          error.response === undefined ? "" : "Détails :" + error.response.data
         }`
       );
     },
