986d1c38a33b431b1f04da2a8c1600ff7d341f50
Louis Delmas <louisde@theodo.fr>
2022-11-17 10:08:19 +0100

    fix: infinite loading namespaces

--- frontend/src/services/api/namespaces.ts
+++ frontend/src/services/api/namespaces.ts
@@ -3,10 +3,17 @@ import { Namespace } from "../../models/tag";
 import { toUrlParams } from "../../utils/urlParam";
 
 export const namespaces = {
-  list: (params, axiosConfig = {}) => {
+  list: (params, page = 1, axiosConfig = {}) => {
     params["page_size"] = 1000;
     return unwrap<PaginatedResponse<Namespace[]>>(
-      apiService.get("/tags/namespaces/" + toUrlParams(params), axiosConfig)
+      apiService.get(
+        "/tags/namespaces/" +
+          toUrlParams({
+            ...params,
+            page: page,
+          }),
+        axiosConfig
+      )
     );
   },
   create: ({ name, scoped_to_model, scoped_to_pk }) =>
