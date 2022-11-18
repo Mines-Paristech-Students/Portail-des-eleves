d5e369676cebd4220202bc17efb21d20757257cc
Louis Delmas <louisde@theodo.fr>
2022-11-18 15:05:08 +0100

    feat: work in progress pages

--- frontend/src/components/settings/users/SettingsUsers.tsx
+++ frontend/src/components/settings/users/SettingsUsers.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { WorkInProgress } from "../../utils/WorkInProgress";
 import { SettingsLayout } from "../SettingsLayout";
 
-export const SettingsUsers = () => <SettingsLayout>Users</SettingsLayout>;
+export const SettingsUsers = () => (
+  <SettingsLayout>
+    <WorkInProgress />
+  </SettingsLayout>
+);
